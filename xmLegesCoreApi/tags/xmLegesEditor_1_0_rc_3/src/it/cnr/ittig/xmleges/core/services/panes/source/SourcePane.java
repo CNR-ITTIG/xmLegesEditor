7c793c9c6ff2c5938646d3a9d5f15ac1ce308b36
ittig <ittig@ittig.cnr.it>
2006-02-02 13:50:49 +0000

    This commit was manufactured by cvs2svn to create tag 'xmLegesEditor_1_0_rc_3'.
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@251 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreApi/tags/xmLegesEditor_1_0_rc_3/src/it/cnr/ittig/xmleges/core/services/panes/source/SourcePane.java
@@ -0,0 +1,25 @@
+package it.cnr.ittig.xmleges.core.services.panes.source;
+
+import it.cnr.ittig.services.manager.Service;
+import it.cnr.ittig.xmleges.core.services.frame.Pane;
+
+/**
+ * Servizio per la visualizzazione del testo sorgente del file XML.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public interface SourcePane extends Service, Pane {
+
+}
