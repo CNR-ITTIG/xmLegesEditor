8062a30730826f569f8be7573bdd514a99ac96b4
Agnoloni <tommaso.agnoloni@gmail.com>
2006-05-19 08:37:36 +0000

    Aggoiunto il componente Dom per la gestione dei riferimenti incompleti
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@61 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/dom/rifincompleti/RifIncompleti.java
@@ -0,0 +1,79 @@
+package it.cnr.ittig.xmleges.editor.services.dom.rifincompleti;
+
+import it.cnr.ittig.services.manager.Service;
+import it.cnr.ittig.xmleges.editor.services.util.urn.Urn;
+
+import org.w3c.dom.Node;
+
+/**
+ * Servizio per la correzione o l'annullamento di un riferimento incompleto
+ * di un
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni</a>
+ */
+public interface RifIncompleti extends Service {
+
+	/**
+	 * Indica se &egrave; possibile sostituire un nodo <code>&lt;?rif&gt;</code> incompleto con un nodo <code>&lt;rif&gt;</code>
+	 * 
+	 * @param node nodo di riferimento
+	 * @return <code>true</code> se &egrave; possibile inserire un nodo
+	 *         <code>&lt;rif&gt;</code>
+	 */
+	public boolean canFix(Node node);
+
+	
+	/**
+	 * Inserisce un nodo <code>&lt;rif&gt; all'interno del nodo
+	 * <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @param start inizio della selezione
+	 * @param end fine della selezione
+	 * @param Urn del riferimenti
+	 * @return <code>null</code> se la modifica non &egrave; riuscita; nodo
+	 *         modificato
+	 */
+	public Node setRif(Node node, int start, int end, Urn urn);
+	
+	
+	/**
+	 * Sostituisce al nodo <code>&lt;?rif&gt;</code> incompleto il suo contenuto testuale.
+	 * 
+	 * @param node nodo di riferimento
+	 * @param start inizio della selezione
+	 * @param end fine della selezione
+	 * @param plainText testo piatto del rif Incompleto
+	 * @return <code>null</code> se la modifica non &egrave; riuscita; nodo
+	 *         modificato
+	 */
+	public Node setPlainText(Node node, int start, int end, String plainText);
+	
+	/**
+	 * Restituisce la sottostringa contenente la Urn dal nodo <code>&lt;?rif&gt;</code> incompleto
+	 * @param node
+	 * @return
+	 */
+	public String getUrn(Node node);
+	
+	/**
+	 * Restituisce la sottostringa contenente la form testuale dal nodo <code>&lt;?rif&gt;</code> incompleto
+	 * @param node
+	 * @return
+	 */
+	public String getText(Node node);
+
+	
+}
