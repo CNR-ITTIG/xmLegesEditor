17563e5bb255bc9f3f38a9ef92be158c78a7816f
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2006-10-20 15:22:38 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@686 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/form/browser/BrowserForm.java
@@ -0,0 +1,70 @@
+package it.cnr.ittig.xmleges.editor.services.form.browser;
+
+import java.net.URL;
+
+import it.cnr.ittig.services.manager.Service;
+import it.cnr.ittig.xmleges.core.services.form.CommonForm;
+
+/**
+ * Servizio per presentare un oggetto grafico per l'apertura di un browser.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ */
+public interface BrowserForm extends Service, CommonForm {
+
+	/**
+	 * Imposta url del browser.
+	 * 
+	 * @param url
+	 *            url da visualizzare.
+	 */
+	public void setUrl(URL url);
+
+	/**
+	 * Imposta un listener su un particolare url che se il browser raggiunge,
+	 * 
+	 * @param url da notificare.
+	 */
+	public void setUrlListener(String url);	
+
+	
+	/**
+	 * Imposta url del browser.
+	 * 
+	 * @param url
+	 *            url da visualizzare.
+	 */
+	public void setUrl(String url);		
+	/**
+	 * Restituisce la url corrente del browser.
+	 * 
+	 * @return URL url corrente del browser.
+	 */
+	public URL getUrl();
+
+	/**
+	 * Restituisce l'html visualizzato dal browser.
+	 * 
+	 * @return String corrispondente all'html visualizzato dal browser.
+	 */
+	public String getHtml();
+		
+	/**
+	 * Imposta le dimensioni della finestra del browser
+	 * 
+	 * @param int width e height
+	 */
+	public void setSize(int width, int height);
+
+	// ALTRI metodi esposti dalla libreria Jdic
+}
