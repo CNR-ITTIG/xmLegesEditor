491533e0e44176f12a36e0e5c96943f10123daf7
Agnoloni <tommaso.agnoloni@gmail.com>
2006-11-14 18:26:54 +0000

    ripuliti gli import
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@789 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesCoreApi/trunk/src/it/cnr/ittig/xmleges/core/util/dom/UtilDom.java
+++ xmLegesCoreApi/trunk/src/it/cnr/ittig/xmleges/core/util/dom/UtilDom.java
@@ -27,7 +27,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.ProcessingInstruction;
 import org.w3c.dom.traversal.DocumentTraversal;
 import org.w3c.dom.traversal.NodeFilter;
 import org.w3c.dom.traversal.NodeIterator;
