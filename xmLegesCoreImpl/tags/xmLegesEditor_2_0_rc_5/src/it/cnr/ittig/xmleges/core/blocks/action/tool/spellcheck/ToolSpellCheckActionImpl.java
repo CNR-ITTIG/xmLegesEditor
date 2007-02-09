40b362c8d643da7a22bc0231b67cbef42eb4770b
Agnoloni <tommaso.agnoloni@gmail.com>
2007-02-09 16:35:54 +0000

    versione rc5
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1011 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreImpl/tags/xmLegesEditor_2_0_rc_5/src/it/cnr/ittig/xmleges/core/blocks/action/tool/spellcheck/ToolSpellCheckActionImpl.java
@@ -0,0 +1,242 @@
+package it.cnr.ittig.xmleges.core.blocks.action.tool.spellcheck;
+
+import it.cnr.ittig.services.manager.Initializable;
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.services.manager.Startable;
+import it.cnr.ittig.xmleges.core.services.action.ActionManager;
+import it.cnr.ittig.xmleges.core.services.action.tool.spellcheck.ToolSpellCheckAction;
+import it.cnr.ittig.xmleges.core.services.bars.Bars;
+import it.cnr.ittig.xmleges.core.services.bars.StatusBar;
+import it.cnr.ittig.xmleges.core.services.document.DocumentClosedEvent;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentOpenedEvent;
+import it.cnr.ittig.xmleges.core.services.event.EventManager;
+import it.cnr.ittig.xmleges.core.services.event.EventManagerListener;
+import it.cnr.ittig.xmleges.core.services.preference.PreferenceManager;
+import it.cnr.ittig.xmleges.core.services.spellcheck.SpellCheck;
+import it.cnr.ittig.xmleges.core.services.spellcheck.dom.DomSpellCheck;
+import it.cnr.ittig.xmleges.core.services.spellcheck.dom.DomSpellCheckEvent;
+import it.cnr.ittig.xmleges.core.services.spellcheck.dom.form.SpellCheckForm;
+import it.cnr.ittig.xmleges.core.services.util.msg.UtilMsg;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.util.EventObject;
+
+import javax.swing.AbstractAction;
+
+import org.w3c.dom.Node;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.action.tool.spellcheck.ToolSpellCheckAction</code>.
+ * </h1>
+ * <h1>Descrizione</h1>
+ * Servizio per l'avvio del controllo ortografico e l'attivazione del controllo
+ * ortografico automatico.
+ * <h1>Configurazione</h1>
+ * Nessuna
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.action.ActionManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.dom.DomSpellCheck:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.util.msg.utilMsg:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.bars.Bars:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.preference.PreferenceManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.dom.form.SpellCheckForm:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.event.EventManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.document.DocumentManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.SpellCheck:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li><code>tool.spellcheck</code>: descrizione dell'azione come specificato nell'ActionManager; </li>
+ * <li><code>tool.spellcheck.auto</code>: descrizione dell'azione come specificato nell'ActionManager; </li> 
+ * <li><code>tool.spellcheck.auto.status.enable</code>: messaggio di abilitazione spellcheck;</li>
+ * <li><code>tool.spellcheck.auto.status.disable</code>: messaggio di disabilitazione spellcheck;</li> 
+ * <li><code>tool.spellcheck.auto.msg.enable</code>: messaggio di abilitazione spellcheck;</li> 
+ * <li><code>tool.spellcheck.auto.msg.disable</code>: messaggio di abilitazione spellcheck;</li>
+ * </ul>
+ * 
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e Tecniche
+ * dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General Public
+ * License </a></dd>
+ * </dl>
+
+ *  * <li>it.cnr.ittig.xmleges.editor.services.action.ActionManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.dom.DomSpellCheck:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.util.msg.utilMsg:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.bars.Bars:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.preference.PreferenceManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.dom.form.SpellCheckForm:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.event.EventManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.document.DocumentManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.spellcheck.SpellCheck:1.0</li>
+
+ * @see it.cnr.ittig.xmleges.core.services.action.ActionManager
+ * @see it.cnr.ittig.xmleges.core.services.preference.PreferenceManager
+ * @see it.cnr.ittig.xmleges.core.services.event.EventManager
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public class ToolSpellCheckActionImpl implements ToolSpellCheckAction, Loggable, Serviceable, Initializable, EventManagerListener, Startable {
+
+	Logger logger;
+
+	ActionManager actionManager;
+
+	DomSpellCheck domSpellCheck;
+
+	SpellCheck spellCheck;
+	
+	UtilMsg utilMsg;
+
+	Bars bars;
+
+	AbstractAction spellAction = new SpellAction();
+
+	StatusBar statusBar;
+
+	PreferenceManager preferenceManager;
+
+	SpellCheckForm spellCheckForm;
+
+	EventManager eventManager;
+
+	DocumentManager documentManager;
+
+	Node activeNode;
+
+	int start, end;
+
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		actionManager = (ActionManager) serviceManager.lookup(ActionManager.class);
+		domSpellCheck = (DomSpellCheck) serviceManager.lookup(DomSpellCheck.class);
+		utilMsg = (UtilMsg) serviceManager.lookup(UtilMsg.class);
+		statusBar = ((Bars) serviceManager.lookup(Bars.class)).getStatusBar();
+		preferenceManager = (PreferenceManager) serviceManager.lookup(PreferenceManager.class);
+		spellCheckForm = (SpellCheckForm) serviceManager.lookup(SpellCheckForm.class);
+		eventManager = (EventManager) serviceManager.lookup(EventManager.class);
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		spellCheck = (SpellCheck) serviceManager.lookup(SpellCheck.class);
+	}
+
+	// ///////////////////////////////////////////////// Initializable Interface
+	public void initialize() throws java.lang.Exception {
+		actionManager.registerAction("tool.spellcheck", spellAction);
+		actionManager.registerAction("tool.spellcheck.auto", new AutoSpellAction());
+		spellAction.setEnabled(false);
+		eventManager.addListener(this, DocumentClosedEvent.class);
+		eventManager.addListener(this, DocumentOpenedEvent.class);
+	}
+
+	// ///////////////////////////////////////////////////// Startable Interface
+	public void start() throws java.lang.Exception {
+		String enable = preferenceManager.getPreferenceAsString(getClass().getName());
+		setAutoSpellCheck(Boolean.valueOf(enable).booleanValue());
+	}
+
+	public void stop() throws java.lang.Exception {
+		preferenceManager.setPreference(getClass().getName(), "" + domSpellCheck.isEnabled());
+	}
+
+	public void manageEvent(EventObject event) {
+		if (event instanceof DocumentOpenedEvent)
+			//controllo se la libreria dello SpellCk. è stata caricata
+			//spellAction.setEnabled(true);
+			spellAction.setEnabled(spellCheck.isLoad());
+
+		if (event instanceof DocumentClosedEvent)
+			spellAction.setEnabled(false);
+
+	}
+
+	// ////////////////////////////////////////// ToolSpellCheckAction Interface
+	public void doSpellCheck() {
+		logger.debug("spellCheck pressed");
+		spellCheckForm.openForm();
+	}
+
+	public void setAutoSpellCheck(boolean auto) {
+		domSpellCheck.setEnabled(auto);
+		statusBar.setText("tool.spellcheck.auto.status." + (auto ? "enable" : "disable"), "spellcheck");
+		if (auto && !documentManager.isEmpty()) {
+			Node nirNode = documentManager.getRootElement();
+			if (nirNode != null) {
+				DomSpellCheckEvent scEvt = new DomSpellCheckEvent(this, domSpellCheck.spellCheck(nirNode));
+				if (scEvt.hasWords())
+					eventManager.fireEvent(scEvt);
+			}
+		}
+	}
+
+	/**
+	 * Azione per attivare la form per il controllo ortografico guidato.
+	 * 
+	 * <p>
+	 * <dl>
+	 * <dt><b>Copyright &copy;: </b></dt>
+	 * <dd>2003 - 2004</dd>
+	 * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+	 * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+	 * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+	 * <dt><b>License: </b></dt>
+	 * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General
+	 * Public License </a></dd>
+	 * </dl>
+	 * 
+	 * @version 1.0
+	 * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+	 * @author <a href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni </a>
+	 */
+	class SpellAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			doSpellCheck();
+		}
+	}
+
+	/**
+	 * Azione per attivare o disabilitare il controllo ortografico automatico.
+	 * 
+	 * <p>
+	 * <dl>
+	 * <dt><b>Copyright &copy;: </b></dt>
+	 * <dd>2003 - 2004</dd>
+	 * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+	 * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+	 * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+	 * <dt><b>License: </b></dt>
+	 * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General
+	 * Public License </a></dd>
+	 * </dl>
+	 * 
+	 * @version 1.0
+	 * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+	 */
+
+	class AutoSpellAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			boolean rin = domSpellCheck.isEnabled();
+			String str = "tool.spellcheck.auto.msg." + (rin ? "disable" : "enable");
+			boolean ris = utilMsg.msgYesNo((Component) e.getSource(), str);
+			setAutoSpellCheck((rin && !ris) || (!rin && ris));
+		}
+	}
+}
---
+++ xmLegesCoreImpl/tags/xmLegesEditor_2_0_rc_5/src/it/cnr/ittig/xmleges/core/blocks/action/tool/unmark/UnmarkActionImpl.java
@@ -0,0 +1,206 @@
+package it.cnr.ittig.xmleges.core.blocks.action.tool.unmark;
+
+import it.cnr.ittig.services.manager.Initializable;
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.action.ActionManager;
+import it.cnr.ittig.xmleges.core.services.action.tool.unmark.UnmarkAction;
+import it.cnr.ittig.xmleges.core.services.document.DocumentClosedEvent;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManagerException;
+import it.cnr.ittig.xmleges.core.services.document.DocumentOpenedEvent;
+import it.cnr.ittig.xmleges.core.services.document.EditTransaction;
+import it.cnr.ittig.xmleges.core.services.dom.extracttext.ExtractText;
+import it.cnr.ittig.xmleges.core.services.dtd.DtdRulesManager;
+import it.cnr.ittig.xmleges.core.services.dtd.DtdRulesManagerException;
+import it.cnr.ittig.xmleges.core.services.event.EventManager;
+import it.cnr.ittig.xmleges.core.services.event.EventManagerListener;
+import it.cnr.ittig.xmleges.core.services.selection.SelectionChangedEvent;
+import it.cnr.ittig.xmleges.core.services.selection.SelectionManager;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+
+import java.awt.event.ActionEvent;
+import java.util.EventObject;
+
+import javax.swing.AbstractAction;
+
+import org.w3c.dom.Node;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.action.testo</code>.</h1>
+ * <h1>Descrizione</h1>
+ * <h1>Configurazione</h1>
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.form.Form:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li>xxx.yyy:</li>
+ * </ul>
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public class UnmarkActionImpl implements UnmarkAction, EventManagerListener, Loggable, Serviceable, Initializable {
+
+	Logger logger;
+
+	int onlyTag = 0;
+
+	int tree = 0;
+
+	ActionManager actionManager;
+
+	EventManager eventManager;
+
+	SelectionManager selectionManager;
+	
+	DocumentManager documentManager;
+	
+	DtdRulesManager dtdRulesManager;
+	
+	ExtractText extractText;
+
+	Node activeNode;
+	
+	String selectedText="";
+
+	doUnmarkAction unmarkaction = new doUnmarkAction ();
+
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		actionManager = (ActionManager) serviceManager.lookup(ActionManager.class);
+		eventManager = (EventManager) serviceManager.lookup(EventManager.class);
+		selectionManager = (SelectionManager) serviceManager.lookup(SelectionManager.class);
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		dtdRulesManager = (DtdRulesManager) serviceManager.lookup(DtdRulesManager.class);
+		extractText = (ExtractText) serviceManager.lookup(ExtractText.class);
+	}
+
+	// ///////////////////////////////////////////////// Initializable Interface
+	public void initialize() throws java.lang.Exception {
+		
+		actionManager.registerAction("edit.unmark", unmarkaction);
+		eventManager.addListener(this, SelectionChangedEvent.class);
+		eventManager.addListener(this, DocumentOpenedEvent.class);
+		eventManager.addListener(this, DocumentClosedEvent.class);
+		enableAction();
+	}
+
+	public void enableAction() {
+		if (activeNode == null) {
+				unmarkaction.setEnabled(false);
+		} else {
+			logger.debug("START enableUnmark");
+			unmarkaction.setEnabled(extractText.canExtractText(activeNode,0,selectedText.length()));
+			logger.debug("END enableUnmark");
+		}
+	}
+
+
+	// ////////////////////////////////////////// EventManagerListener Interface
+	public void manageEvent(EventObject event) {
+
+		if (event instanceof SelectionChangedEvent) {
+			if (((SelectionChangedEvent) event).isActiveNodeChanged()) {
+				logger.debug("selectionChangedEvent: " + ((SelectionChangedEvent) event).toString());
+				activeNode = ((SelectionChangedEvent) event).getActiveNode();
+				
+				if (activeNode != null) {
+					
+					logger.debug("active node" + activeNode + " node type " + activeNode.getNodeType());
+					
+					//   CERCA  SELECTEDTEXT  ////////////////////////////////////////
+					if(activeNode.getNodeValue()==null){
+						if(UtilDom.getTextNode(activeNode)==null || UtilDom.getTextNode(activeNode).trim().equals(""))
+							selectedText=activeNode.getNodeName();
+						else
+							selectedText=UtilDom.getTextNode(activeNode);
+					}
+					else				
+						selectedText=activeNode.getNodeValue();
+					//////////////////////////////////////////////////////////////////
+					
+					
+					//  ENABLE ACTION  /////////////////////////////////////////////////
+					if ((UtilDom.isTextNode(activeNode))) {     // AZIONE SU NODO TESTO
+						logger.debug("azione sul nodo testo");
+						enableAction();
+					} else 
+						unmarkaction.setEnabled(false);
+					// /////////////////////////////////////////////////////////////////
+					
+				} else 
+					unmarkaction.setEnabled(false);
+			}
+		} else if (event instanceof DocumentClosedEvent || event instanceof DocumentOpenedEvent) {
+			unmarkaction.setEnabled(false);
+		}
+	}
+
+	
+	public void doUnmark() {	
+		Node modificato = activeNode;
+		//	appiattisce il testo				
+		Node extractedNode;
+		extractedNode = extractText.extractText(modificato,0,selectedText.length());
+		try{
+			EditTransaction tr = documentManager.beginEdit();
+			// ripristinata la dipendenza da extractText con Transazione;
+			// due transazioni separate fra estrazione e cancellazionealtrimenti da'problemi sugli update
+			//extractedNode = extractText.extractTextDOM(modificato,0,selectedText.length());
+			if(extractedNode!=null && extractedNode.getPreviousSibling()!=null){
+				
+				try{
+					if(extractedNode.getPreviousSibling().getChildNodes().getLength()==0  &&
+					   dtdRulesManager.queryCanDelete(extractedNode.getParentNode(),extractedNode.getPreviousSibling()))
+							extractedNode.getParentNode().removeChild(extractedNode.getPreviousSibling());
+				}
+				catch(DtdRulesManagerException ex){	
+				}
+				
+    			Node toSelect=extractedNode.getParentNode();
+				UtilDom.mergeTextNodes(extractedNode.getParentNode());
+				documentManager.commitEdit(tr);
+				selectionManager.setActiveNode(this, toSelect);
+				
+			}else{
+				documentManager.rollbackEdit(tr);
+			}
+			
+		}catch (DocumentManagerException ex) {
+			logger.error(ex.getMessage(), ex);
+			return;
+		
+		}					
+	}
+
+	public class doUnmarkAction extends AbstractAction {
+
+		public void actionPerformed(ActionEvent e) {
+			doUnmark();
+		}
+	}
+
+	
+	}
