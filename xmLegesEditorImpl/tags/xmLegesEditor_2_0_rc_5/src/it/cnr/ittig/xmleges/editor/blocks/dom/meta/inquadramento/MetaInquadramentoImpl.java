f0009fda2a48bd92ef4d8bcd1d30388e37e46ce0
Agnoloni <tommaso.agnoloni@gmail.com>
2007-03-09 17:18:20 +0000

    versione rc5; last NIR2.1
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1039 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorImpl/tags/xmLegesEditor_2_0_rc_5/src/it/cnr/ittig/xmleges/editor/blocks/dom/meta/inquadramento/MetaInquadramentoImpl.java
@@ -0,0 +1,470 @@
+package it.cnr.ittig.xmleges.editor.blocks.dom.meta.inquadramento;
+
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManagerException;
+import it.cnr.ittig.xmleges.core.services.document.EditTransaction;
+import it.cnr.ittig.xmleges.core.services.util.rulesmanager.UtilRulesManager;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+import it.cnr.ittig.xmleges.editor.services.dom.meta.inquadramento.InfoMancanti;
+import it.cnr.ittig.xmleges.editor.services.dom.meta.inquadramento.Infodoc;
+import it.cnr.ittig.xmleges.editor.services.dom.meta.inquadramento.MetaInquadramento;
+import it.cnr.ittig.xmleges.editor.services.dom.meta.inquadramento.Oggetto;
+import it.cnr.ittig.xmleges.editor.services.dom.rinumerazione.Rinumerazione;
+import it.cnr.ittig.xmleges.editor.services.util.dom.NirUtilDom;
+
+import java.util.Vector;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class MetaInquadramentoImpl implements MetaInquadramento, Loggable, Serviceable {
+
+	Logger logger;
+
+	DocumentManager documentManager;
+
+	UtilRulesManager utilRulesManager;
+	
+	Rinumerazione rinumerazione; 
+	
+	NirUtilDom nirUtilDom;	
+	
+	Node node=null;
+
+	public void setInfodoc(Infodoc infodoc) {
+		if(infodoc!=null){
+			Document doc = documentManager.getDocumentAsDom();
+			try {
+				EditTransaction tr = documentManager.beginEdit();
+				if (setDOMInfodoc(infodoc)) {
+					rinumerazione.aggiorna(doc);
+					documentManager.commitEdit(tr);
+				} else
+					documentManager.rollbackEdit(tr);
+				} catch (DocumentManagerException ex) {
+					logger.error(ex.toString() + " DocumentManagerException in CiclodiVita");
+				}
+		}
+	}
+		
+		
+
+	private boolean setDOMInfodoc(Infodoc infodoc) {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		Node inquadramentoNode = UtilDom.findRecursiveChild(activeMeta,"inquadramento");
+		
+		boolean missingInquadramento = false;
+		
+		if (inquadramentoNode==null){
+			inquadramentoNode = doc.createElement("inquadramento");
+			missingInquadramento = true;
+		}
+					
+		Element infodoc_element = doc.createElement("infodoc");
+		if((infodoc.getNatura()!=null)&&(!infodoc.getNatura().trim().equals("")))
+			infodoc_element.setAttribute("natura", infodoc.getNatura());
+		if((infodoc.getNormativa()!=null)&&(!infodoc.getNormativa().trim().equals("")))
+			infodoc_element.setAttribute("normativa", infodoc.getNormativa());
+		if((infodoc.getFunzione()!=null)&&(!infodoc.getFunzione().trim().equals("")))
+			infodoc_element.setAttribute("funzione", infodoc.getFunzione());
+		if(infodoc.getFonte()!=null)
+			infodoc_element.setAttribute("fonte", infodoc.getFonte());
+			
+		Node infodocNode = (Node)infodoc_element;
+		
+		Node oldTag = UtilDom.findRecursiveChild(activeMeta,"infodoc");
+		if (oldTag!=null) // c'era gia' un nodo infodoc
+			inquadramentoNode.replaceChild(infodocNode, oldTag);
+		else 
+			utilRulesManager.orderedInsertChild(inquadramentoNode,infodocNode);
+		
+		
+		if(missingInquadramento){			
+			utilRulesManager.orderedInsertChild(activeMeta,inquadramentoNode);
+		}
+		
+		return true;
+
+	}
+
+
+
+	public void setInfomancanti(InfoMancanti infomancanti) {
+		if(infomancanti!=null){
+			Document doc = documentManager.getDocumentAsDom();
+			try {
+				EditTransaction tr = documentManager.beginEdit();
+				if (setDOMInfomancanti(infomancanti)) {
+					rinumerazione.aggiorna(doc);
+					documentManager.commitEdit(tr);
+				} else
+					documentManager.rollbackEdit(tr);
+				} catch (DocumentManagerException ex) {
+					logger.error(ex.toString() + " DocumentManagerException in CiclodiVita");
+				}
+		}
+
+
+	}
+
+	private boolean setDOMInfomancanti(InfoMancanti infomancanti) {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		
+		Node inquadramentoNode = UtilDom.findRecursiveChild(activeMeta,"inquadramento");
+		boolean missingInquadramento = false;
+		
+		if (inquadramentoNode==null){
+			inquadramentoNode = doc.createElement("inquadramento");
+			missingInquadramento = true;
+		}
+		
+		Node infomancantiNode = utilRulesManager.getNodeTemplate("infomancanti");
+
+		
+		Element infomancanti_element=null;
+		if(infomancanti.getMTitolodoc()!= null && !infomancanti.getMTitolodoc().trim().equals("")){
+			infomancanti_element = (Element) utilRulesManager.getNodeTemplate("mTitolodoc");
+			UtilDom.setAttributeValue(infomancanti_element,"value",infomancanti.getMTitolodoc());			
+			utilRulesManager.orderedInsertChild(infomancantiNode,infomancanti_element);
+	
+		}
+		if(infomancanti.getMTipodoc()!= null && !infomancanti.getMTipodoc().trim().equals("")){
+			infomancanti_element = (Element) utilRulesManager.getNodeTemplate("mTipodoc");
+			UtilDom.setAttributeValue(infomancanti_element,"value",infomancanti.getMTipodoc() );
+			utilRulesManager.orderedInsertChild(infomancantiNode,infomancanti_element);
+		}
+		if(infomancanti.getMDatadoc()!= null && !infomancanti.getMDatadoc().trim().equals("")){
+			infomancanti_element = (Element) utilRulesManager.getNodeTemplate("mDatadoc");
+			UtilDom.setAttributeValue(infomancanti_element,"value",infomancanti.getMDatadoc() );
+			utilRulesManager.orderedInsertChild(infomancantiNode,infomancanti_element);
+		}
+		if(infomancanti.getMNumdoc()!= null && !infomancanti.getMNumdoc().trim().equals("")){
+			infomancanti_element = (Element) utilRulesManager.getNodeTemplate("mNumdoc");
+			UtilDom.setAttributeValue(infomancanti_element,"value",infomancanti.getMNumdoc() );
+			utilRulesManager.orderedInsertChild(infomancantiNode,infomancanti_element);
+		}
+		if(infomancanti.getMEmanante()!= null && !infomancanti.getMEmanante().trim().equals("")){
+			infomancanti_element = (Element) utilRulesManager.getNodeTemplate("mEmanante");
+			UtilDom.setAttributeValue(infomancanti_element,"value",infomancanti.getMEmanante() );
+			utilRulesManager.orderedInsertChild(infomancantiNode,infomancanti_element);
+		}
+		/////////////////////
+		Node oldTag = UtilDom.findRecursiveChild(activeMeta,"infomancanti");
+		if (oldTag!=null) // c'era gia' un nodo infomancanti
+			inquadramentoNode.replaceChild(infomancantiNode, oldTag);
+		else 
+			utilRulesManager.orderedInsertChild(inquadramentoNode,infomancantiNode);
+		
+		if(missingInquadramento){
+			
+			utilRulesManager.orderedInsertChild(activeMeta,inquadramentoNode);
+		}
+				
+		return true;
+	}
+
+
+
+	public void setOggetto(Oggetto oggetto) {
+		if(oggetto!=null){
+			Document doc = documentManager.getDocumentAsDom();
+			try {
+				EditTransaction tr = documentManager.beginEdit();
+				if (setDOMOggetto(oggetto)) {
+					rinumerazione.aggiorna(doc);
+					documentManager.commitEdit(tr);
+				} else
+					documentManager.rollbackEdit(tr);
+				} catch (DocumentManagerException ex) {
+					logger.error(ex.toString() + " DocumentManagerException in CiclodiVita");
+				}
+		}
+
+
+	}
+
+	private boolean setDOMOggetto(Oggetto oggetto) {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		Node inquadramentoNode = UtilDom.findRecursiveChild(activeMeta,"inquadramento");
+		boolean missingInquadramento = false;
+		
+		if (inquadramentoNode==null){
+			inquadramentoNode = doc.createElement("inquadramento");
+			missingInquadramento = true;
+		}
+		
+		Node oggettoNode = utilRulesManager.getNodeTemplate("oggetto");
+		
+		Element oggetto_element=null;
+		if(oggetto.getFinalita()!= null && !oggetto.getFinalita().trim().equals("")){
+			oggetto_element = (Element) utilRulesManager.getNodeTemplate("finalita");
+			UtilDom.setAttributeValue(oggetto_element,"value",oggetto.getFinalita() );
+			utilRulesManager.orderedInsertChild(oggettoNode,oggetto_element);
+		}
+		if(oggetto.getDestinatario()!= null && !oggetto.getDestinatario().trim().equals("")){
+			oggetto_element = (Element) utilRulesManager.getNodeTemplate("destinatario");
+			UtilDom.setAttributeValue(oggetto_element,"value",oggetto.getDestinatario() );
+			utilRulesManager.orderedInsertChild(oggettoNode,oggetto_element);
+		}
+		if(oggetto.getTerritorio()!= null && !oggetto.getTerritorio().trim().equals("")){
+			oggetto_element = (Element) utilRulesManager.getNodeTemplate("territorio");
+			UtilDom.setAttributeValue(oggetto_element,"value",oggetto.getTerritorio() );
+			utilRulesManager.orderedInsertChild(oggettoNode,oggetto_element);
+		}
+		if(oggetto.getAttivita()!= null && !oggetto.getAttivita().trim().equals("")){
+			oggetto_element = (Element) utilRulesManager.getNodeTemplate("attivita");
+			UtilDom.setAttributeValue(oggetto_element,"value",oggetto.getAttivita() );
+			utilRulesManager.orderedInsertChild(oggettoNode,oggetto_element);
+		}
+		
+		Node oldTag = UtilDom.findRecursiveChild(activeMeta,"oggetto");
+		if (oldTag!=null) // c'era gia' un nodo oggetto
+			inquadramentoNode.replaceChild(oggettoNode, oldTag);
+		else 
+			utilRulesManager.orderedInsertChild(inquadramentoNode,oggettoNode);
+		
+		
+		if(missingInquadramento){
+			
+			utilRulesManager.orderedInsertChild(activeMeta,inquadramentoNode);
+		}
+		
+		
+				
+		return true;
+		
+				
+		
+	}
+
+
+
+	public void setProponenti(String[] proponenti) {
+		if((proponenti!=null) && (proponenti.length >0)){
+			Document doc = documentManager.getDocumentAsDom();
+			try {
+				EditTransaction tr = documentManager.beginEdit();
+				if (setDOMProponenti(proponenti)) {
+					rinumerazione.aggiorna(doc);
+					documentManager.commitEdit(tr);
+				} else
+					documentManager.rollbackEdit(tr);
+				} catch (DocumentManagerException ex) {
+					logger.error(ex.toString() + " DocumentManagerException in CiclodiVita");
+			}
+		}
+
+
+	}
+
+	private boolean setDOMProponenti(String[] proponenti) {
+			
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		Node inquadramentoNode = UtilDom.findRecursiveChild(activeMeta,"inquadramento");
+		boolean missingInquadramento = false;
+		
+		if (inquadramentoNode==null){
+			inquadramentoNode = doc.createElement("inquadramento");
+			missingInquadramento = true;
+		}
+		
+		Node proponentiNode = utilRulesManager.getNodeTemplate("proponenti");
+		
+		Element proponenti_element=null;
+		if(proponenti!= null && proponenti.length>0 ){
+			for (int i = 0; i < proponenti.length; i++) {
+				proponenti_element =  (Element) utilRulesManager.getNodeTemplate("proponente");
+				UtilDom.setAttributeValue(proponenti_element,"value",proponenti[i] );
+				utilRulesManager.orderedInsertChild(proponentiNode,proponenti_element);
+			}
+		}
+		
+		Node oldTag = UtilDom.findRecursiveChild(activeMeta,"proponenti");
+		if (oldTag!=null) // c'era gia' un nodo proponenti
+			inquadramentoNode.replaceChild(proponentiNode, oldTag);
+		else 
+			utilRulesManager.orderedInsertChild(inquadramentoNode,proponentiNode);
+		
+		
+		if(missingInquadramento){
+			
+			utilRulesManager.orderedInsertChild(activeMeta,inquadramentoNode);
+		}
+		
+				
+		return true;
+	
+	}
+
+
+
+	public Infodoc getInfodoc() {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		
+		String natura=null;		
+		String normativa=null;		
+		String funzione=null;		
+		String fonte=null;
+
+		
+		Node infodocNode = UtilDom.findRecursiveChild(activeMeta,"infodoc");
+		if (infodocNode!=null) {
+			Node n = infodocNode;
+			natura = n.getAttributes().getNamedItem("natura") != null ? n.getAttributes().getNamedItem("natura").getNodeValue() : null;
+			normativa = n.getAttributes().getNamedItem("normativa") != null ? n.getAttributes().getNamedItem("normativa").getNodeValue() : null;
+			funzione = n.getAttributes().getNamedItem("funzione") != null ? n.getAttributes().getNamedItem("funzione").getNodeValue() : null;
+			fonte = n.getAttributes().getNamedItem("fonte") != null ? n.getAttributes().getNamedItem("fonte").getNodeValue() : null;
+		}
+		return (new Infodoc(natura, normativa, funzione, fonte));
+
+	}
+
+	public InfoMancanti getInfomancanti() {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		String mTitolodoc=null;		
+		String mTipodoc=null;		
+		String mDatadoc=null;		
+		String mNumdoc=null;		
+		String mEmanante=null;
+
+		Node infomancNode = UtilDom.findRecursiveChild(activeMeta,"infomancanti");
+		if (infomancNode!=null) {
+			
+			Node n = infomancNode;
+			
+			NodeList infomanc_elementList = n.getChildNodes();
+			for (int i = 0; i < infomanc_elementList.getLength();i++) {
+				String valore=UtilDom.getAttributeValueAsString(infomanc_elementList.item(i),"value");
+				
+				if(infomanc_elementList.item(i).getNodeName().equals("mTitolodoc"))
+					mTitolodoc=valore;					
+				if(infomanc_elementList.item(i).getNodeName().equals("mTipodoc"))					
+					mTipodoc=valore;
+				if(infomanc_elementList.item(i).getNodeName().equals("mDatadoc"))
+					mDatadoc = valore;
+				if(infomanc_elementList.item(i).getNodeName().equals("mNumdoc"))
+					mNumdoc = valore;
+				if(infomanc_elementList.item(i).getNodeName().equals("mEmanante"))
+					mEmanante = valore;
+					
+			}
+			return (new InfoMancanti(mTitolodoc, mTipodoc, mDatadoc, mNumdoc,mEmanante));
+		}else return null;
+		
+		
+	}
+
+	public Oggetto getOggetto() {
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		String finalita=null;		
+		String destinatario=null;		
+		String territorio=null;		
+		String attivita=null;
+
+
+		Node oggettoNode = UtilDom.findRecursiveChild(activeMeta,"oggetto");
+		if (oggettoNode!=null) {
+			
+			Node n = oggettoNode;
+			
+			NodeList oggetto_elementList = n.getChildNodes();
+			for (int i = 0; i < oggetto_elementList.getLength();i++) {
+				String valore=UtilDom.getAttributeValueAsString(oggetto_elementList.item(i),"value");
+				if(oggetto_elementList.item(i).getNodeName().equals("finalita"))
+					finalita = valore;  
+				if(oggetto_elementList.item(i).getNodeName().equals("destinatario"))
+					destinatario = valore;
+				if(oggetto_elementList.item(i).getNodeName().equals("territorio"))
+					territorio = valore;
+				if(oggetto_elementList.item(i).getNodeName().equals("attivita"))
+					attivita = valore;
+									
+			}
+			
+			return (new Oggetto(finalita, destinatario, territorio, attivita));
+		}else
+			return null;
+		
+		
+		
+
+	}
+
+	public String[] getProponenti() {
+		
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		Vector proponentiVect = new Vector();
+		Node proponentiNode = UtilDom.findRecursiveChild(activeMeta,"proponenti");
+		
+		if (proponentiNode!=null) {
+					
+			Node n = proponentiNode;			
+			NodeList proponenti_elementList = n.getChildNodes();
+			for (int i = 0; i < proponenti_elementList.getLength();i++) {
+				String valore=UtilDom.getAttributeValueAsString(proponenti_elementList.item(i),"value");
+				if(proponenti_elementList.item(i).getNodeName().equals("proponente"))
+					proponentiVect.add(valore);
+				}
+		}
+		
+		
+		String[] proponenti = new String[proponentiVect.size()];
+		proponentiVect.copyInto(proponenti);
+		return proponenti;
+
+	}
+
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+
+	}
+
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		rinumerazione = (Rinumerazione) serviceManager.lookup(Rinumerazione.class);
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);		
+		utilRulesManager = (UtilRulesManager) serviceManager.lookup(UtilRulesManager.class);
+		nirUtilDom = (NirUtilDom) serviceManager.lookup(NirUtilDom.class);
+		
+	}
+	
+	/**
+	 * Rimuove i tag con un determinato nome
+	 */
+	private void removeMetaByName(String nome, Node node) {
+		Document doc = documentManager.getDocumentAsDom();
+		Node toRemove;
+		
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+	
+		do {
+			toRemove = UtilDom.findRecursiveChild(activeMeta,nome); 
+			if (toRemove != null) {
+				toRemove.getParentNode().removeChild(toRemove);
+			}
+		} while (toRemove != null);
+	}
+
+
+
+	public void setActiveNode(Node node) {
+		this.node=node;
+		
+	}
+
+
+}
---
+++ xmLegesEditorImpl/tags/xmLegesEditor_2_0_rc_5/src/it/cnr/ittig/xmleges/editor/blocks/dom/xmleges/linker/XmLegesLinkerImpl.java
@@ -0,0 +1,258 @@
+package it.cnr.ittig.xmleges.editor.blocks.dom.xmleges.linker;
+
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.EditTransaction;
+import it.cnr.ittig.xmleges.core.services.dtd.DtdRulesManager;
+import it.cnr.ittig.xmleges.core.services.frame.Frame;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+import it.cnr.ittig.xmleges.core.util.xml.UtilXml;
+import it.cnr.ittig.xmleges.editor.services.dom.rinumerazione.Rinumerazione;
+import it.cnr.ittig.xmleges.editor.services.dom.xmleges.linker.XmLegesLinker;
+import it.cnr.ittig.xmleges.editor.services.util.dom.NirUtilDom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.dom.xmleges.linker.XmLegesLinker</code>.</h1>
+ * <h1>Descrizione</h1>
+ * <h1>Configurazione</h1>
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.form.Form:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li>xxx.yyy:</li>
+ * </ul>
+ * 
+ * @see
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ * @author <a href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni </a>
+ */
+public class XmLegesLinkerImpl implements XmLegesLinker, Loggable, Serviceable {
+	Logger logger;
+
+	DtdRulesManager dtdRulesManager;
+
+	DocumentManager documentManager;
+
+	NirUtilDom nirUtilDom;
+
+	Rinumerazione rinumerazione;
+
+	Frame frame;
+
+	Node modified = null;
+
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		dtdRulesManager = (DtdRulesManager) serviceManager.lookup(DtdRulesManager.class);
+		nirUtilDom = (NirUtilDom) serviceManager.lookup(NirUtilDom.class);
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		rinumerazione = (Rinumerazione) serviceManager.lookup(Rinumerazione.class);
+		frame = (Frame) serviceManager.lookup(Frame.class);
+	}
+
+	// /////////////////////////////////////////// ParserRiferimenti Interface
+
+	public Node setParsedText(Node nodeSel, String parsedText, String testoPrima, String testoDopo) {
+
+		logger.debug("[ParserRiferimentiImpl: setParsedText" + parsedText);
+		if (parsedText == null || parsedText.length() == 0) {
+			logger.warn("Lunghezza del risultato da inserire: 0");
+			return null;
+		}
+		try {
+			EditTransaction tr = documentManager.beginEdit();
+
+			logger.debug("nodo selezionato:  " + UtilDom.getNodeSummary(nodeSel));
+			Document doc = documentManager.getDocumentAsDom();
+			parsedText = "<?xml version=\"1.0\" encoding=\""+documentManager.getEncoding()+"\"?>"
+					+ "<ris  xmlns:h=\"http://www.w3.org/HTML/1998/html4\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">" + parsedText.trim() + "</ris>";
+
+			Node parsedNodeSel = UtilXml.textToXML(parsedText, true).getDocumentElement();
+			
+			if(logger.isDebugEnabled())
+				logger.debug("parsed: " + UtilDom.domToString(parsedNodeSel));
+
+			NodeList nl = parsedNodeSel.getChildNodes();
+			Node padre = nodeSel.getParentNode();
+			// Modifica Enrico
+			// Prendo il nodo successivo al nodo che contiene la selezione
+			// per poi inserire al posto del nodo selezione la successione dei
+			// nodi creati
+			// Node nextSelNode = nodeSel.getNextSibling();
+			
+			if(logger.isDebugEnabled()){
+				logger.debug("------Inizio---------");
+				logger.debug(UtilDom.domToString(padre));
+			}
+			
+			if (testoPrima.length() > 0)
+				padre.insertBefore(doc.createTextNode(testoPrima), nodeSel);
+			
+			if(logger.isDebugEnabled()){
+				logger.debug("------Dopo inserimento testo prima---------");
+				logger.debug(UtilDom.domToString(padre));
+			}
+			
+			UtilDom.trimTextNode(parsedNodeSel, true);
+
+			while (nl.getLength() > 0) {
+				Node n = parsedNodeSel.removeChild(nl.item(0));
+				Node imp = doc.importNode(n, true);
+				padre.insertBefore(imp, nodeSel);
+			}
+
+			if(logger.isDebugEnabled()){
+				logger.debug("------Dopo inserimento lista nodi risultato---------");
+				logger.debug(UtilDom.domToString(padre));
+			}
+			
+			if (testoDopo.length() > 0)
+				padre.insertBefore(doc.createTextNode(testoDopo), nodeSel);
+			padre.removeChild(nodeSel);
+
+			if(logger.isDebugEnabled()){
+				logger.debug("-----Dopo inserimento testo dopo----------");
+				logger.debug(UtilDom.domToString(padre));
+			}
+			
+			UtilXml.concatenaTextNode(padre);
+
+			modified = padre;
+			
+			if(logger.isDebugEnabled()){
+				logger.debug("----Dopo concatenaTextNode--------");
+				logger.debug(UtilDom.domToString(padre));
+			}
+			
+			// rinumerazione.aggiorna(doc);
+			logger.debug("committed edit in setParsedText");
+			documentManager.commitEdit(tr);
+
+			return modified;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	public Node setParsedDocument(String parsedText) {
+		parsedText = "<?xml version=\"1.0\" encoding=\""+documentManager.getEncoding()+"\"?>" + parsedText;
+		logger.debug("[ParserRiferimentiImpl: setParsedDocument" + parsedText);
+		Document doc = documentManager.getDocumentAsDom();
+		try {
+			Document d = UtilXml.textToXML(parsedText, true);
+			Node imp = doc.importNode(nirUtilDom.getTipoAtto(d),true);
+			UtilDom.trimTextNode(imp, true);
+			EditTransaction tr = documentManager.beginEdit();
+			nirUtilDom.getNIRElement(doc).replaceChild(imp, nirUtilDom.getTipoAtto(doc));
+			modified = nirUtilDom.getTipoAtto(doc);
+			rinumerazione.aggiorna(doc);
+			logger.debug("committed edit in setParsedDocument");
+			documentManager.commitEdit(tr);
+			frame.reloadAllPanes();
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+			return null;
+		}
+		return modified;
+	}
+	
+
+
+	public Node setParsedNode(Node node, String textRis) {
+		logger.debug("[ParserRiferimentiImpl: setParsedNode" + textRis);
+		Document doc = documentManager.getDocumentAsDom();
+		try {
+			EditTransaction tr = documentManager.beginEdit();
+			Node parent = node.getParentNode();
+			String text = textRis;
+			text = "<?xml version=\"1.0\" encoding=\""+documentManager.getEncoding()+"\"?>"
+					+ "<ris  xmlns:h=\"http://www.w3.org/HTML/1998/html4\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">" + text + "</ris>";
+			Node root = UtilXml.textToXML(text, true).getDocumentElement(); 
+			// va
+			// sotto
+			// il
+			// tag
+			// <ris></ris>
+			UtilDom.trimTextNode(root, true);
+
+			NodeList nl = root.getChildNodes();
+			Node imp = doc.importNode(root.getFirstChild(), true);
+			modified = imp;
+			parent.replaceChild(imp, node);
+			Node insAfter = imp;
+			for (int i = 1; i < nl.getLength(); i++) {
+				imp = doc.importNode(nl.item(i), true);
+				parent.insertBefore(imp, insAfter.getNextSibling());
+				insAfter = imp;
+				if (UtilDom.findParentByName(imp, "rif") != null)
+					modified = imp;
+				// logger.debug("setParsedNode: nl.getLength "+nl.getLength());
+			}
+			logger.debug("committed edit in setParsedNode");
+			documentManager.commitEdit(tr);
+
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+		return modified;
+	}
+
+	// public boolean setParsedNodes(Node[] nodes, String[] textRis){
+	// for (int i = 0; i < nodes.length; i++) {
+	// if(! setParsedNode(nodes[i], textRis[i]))
+	// return(false);
+	// }
+	// return true;
+	// }
+	//    
+	public Node setParsedNodes(Node[] nodes, String[] textRis) {
+		Document doc = documentManager.getDocumentAsDom();
+		try {
+			EditTransaction tr = documentManager.beginEdit();
+			for (int i = 0; i < nodes.length; i++) {
+				Node node = nodes[i];
+				Node parent = node.getParentNode();
+				String text = textRis[i];
+				text = "<?xml version=\"1.0\" encoding=\""+documentManager.getEncoding()+"\"?>"
+						+ "<ris  xmlns:h=\"http://www.w3.org/HTML/1998/html4\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">" + text + "</ris>";
+				Node root = UtilXml.textToXML(text, true).getDocumentElement();
+
+				NodeList nl = root.getChildNodes();
+				UtilDom.trimTextNode(root, true);
+				parent.removeChild(node);
+				for (int j = 0; j < nl.getLength(); j++) {
+					Node imp = doc.importNode(nl.item(j), true);
+					parent.appendChild(imp);
+				}
+				modified = parent;
+			}
+
+			// rinumerazione.aggiorna(doc);
+			documentManager.commitEdit(tr);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return null;
+		}
+		return modified;
+	}
+
+}
