013e39f96b2591f5b9c18f83c671492d58c0c17e
Agnoloni <tommaso.agnoloni@gmail.com>
2007-03-09 18:14:24 +0000

    versione rc5; last NIR2.1
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1051 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreApi/tags/xmLegesEditor_2_0_rc_5/src/it/cnr/ittig/xmleges/core/services/form/listtextfield/ListTextFieldEditor.java
@@ -0,0 +1,82 @@
+package it.cnr.ittig.xmleges.core.services.form.listtextfield;
+
+import java.awt.Component;
+import java.awt.Dimension;
+
+/**
+ * Interfaccia necessaria per definire l'editor degli elementi della lista per
+ * <code>ListTextField</code>.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public interface ListTextFieldEditor {
+
+	/**
+	 * Restituisce il componente grafico da visualizzare come editor.
+	 * 
+	 * @return componente grafico da visualizzare
+	 */
+	public Component getAsComponent();
+
+	/**
+	 * Restituisce l'oggetto da inserire nella lista.
+	 * 
+	 * @return oggetto da inserire nella lista, o null per non inserire nessun
+	 *         oggetto.
+	 */
+	public Object getElement();
+	
+//	/**
+//	 * Restituisce gli oggetti da inserire nella lista.
+//	 * 
+//	 * @return oggetti da inserire nella lista, o null per non inserire nessun
+//	 *         oggetto.
+//	 */
+//	public Object[] getElements();
+
+	/**
+	 * Imposta l'oggetto selezionato nella lista che deve essere presentato nei
+	 * campi di modifica.
+	 * 
+	 * @param object
+	 */
+	public void setElement(Object object);
+
+	/**
+	 * Richiede all'editor di pulire i campi
+	 */
+	public void clearFields();
+
+	/**
+	 * Richiede all'editor di controllare che i dati inseriti siano corretti. In
+	 * caso di errore, deve essere possibile chiamare getErrorMessage per
+	 * presentare il messaggio di errore all'utente.
+	 * 
+	 * @return true se i dati inseriti sono corretti, false se sono errati
+	 */
+	public boolean checkData();
+
+	/**
+	 * Richiede all'editor di pulire i campi
+	 */
+	public String getErrorMessage();
+
+	/**
+	 * Richiede all'editor la dimensione desiderata per la form
+	 * 
+	 * @return dimensione per la form, o null per non impostare la dimensione.
+	 */
+	public Dimension getPreferredSize();
+}
