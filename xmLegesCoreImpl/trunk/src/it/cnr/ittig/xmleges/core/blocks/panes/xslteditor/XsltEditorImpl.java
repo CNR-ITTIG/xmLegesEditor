07b792c2061688586cec9ca89cfaa06c598370aa
Agnoloni <tommaso.agnoloni@gmail.com>
2007-06-11 16:38:04 +0000

    usa serializedApplyXslt
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1361 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesCoreImpl/trunk/src/it/cnr/ittig/xmleges/core/blocks/panes/xslteditor/XsltEditorImpl.java
+++ xmLegesCoreImpl/trunk/src/it/cnr/ittig/xmleges/core/blocks/panes/xslteditor/XsltEditorImpl.java
@@ -232,8 +232,9 @@ public class XsltEditorImpl implements XsltEditor, Pane, Loggable, Serviceable, 
 			xsltPane.set(xslt, css, paramPanel.getParams());
 			try {
 				UtilXslt.remove(xslt);	//rimuovo il file altrimenti riprende "il vecchio" dalla cache
-				Node node = UtilXslt.applyXslt(documentManager.getDocumentAsDom(), xslt);
-				convTextArea.setText(UtilDom.domToString(node));
+				//Node node = UtilXslt.applyXslt(documentManager.getDocumentAsDom(), xslt);
+				//convTextArea.setText(UtilDom.domToString(node));
+				convTextArea.setText(UtilXslt.serializedApplyXslt(documentManager.getDocumentAsDom(), xslt));
 			} catch (Exception ex) {
 				// TODO MSG
 				logger.error(ex.toString(), ex);
