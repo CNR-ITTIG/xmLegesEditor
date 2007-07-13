d3293f539983e2c9eb8f53252071eb9598ed2761
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2007-07-13 19:21:58 +0000

    versione rc7
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1429 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreImpl/tags/xmLegesEditor_2_0_rc_7/trunk/src/it/cnr/ittig/xmleges/core/blocks/mswordconverter/win-32/antiword.bat
@@ -0,0 +1,2 @@
+set HOME=antiword\win32
+antiword\win32\antiword\antiword.exe -t -w %1 %2 > %3
---
+++ xmLegesCoreImpl/tags/xmLegesEditor_2_0_rc_7/trunk/src/it/cnr/ittig/xmleges/core/blocks/version/VersionListItem.java
@@ -0,0 +1,88 @@
+package it.cnr.ittig.xmleges.core.blocks.version;
+
+/**
+ * Classe contenitore delle versioni con relative url del file contenente la nuova
+ * versione e sua descrizione.
+ * 
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e Tecniche
+ * dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General Public
+ * License </a></dd>
+ * </dl>
+ * 
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public class VersionListItem {
+	/** Versione */
+	String version = "";
+
+	/** URL del file dell'applicazione */
+	String url = "";
+
+	/** Descrizione della nuova versione */
+	String desc = "";
+
+	/**
+	 * Restituisce la versione.
+	 * 
+	 * @return versione
+	 */
+	public String getVersion() {
+		return this.version;
+	}
+
+	/**
+	 * Imposta la versione.
+	 * 
+	 * @param version versione
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	/**
+	 * Restituisce la URL.
+	 * 
+	 * @return URL
+	 */
+	public String getUrl() {
+		return this.url;
+	}
+
+	/**
+	 * Imposta la URL
+	 * 
+	 * @param url url
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * Restituisce la descrizione.
+	 * 
+	 * @return descrizione
+	 */
+	public String getDescription() {
+		return this.desc;
+	}
+
+	/**
+	 * Imposta la descrizione.
+	 * 
+	 * @param desc descrizione
+	 */
+	public void setDescription(String desc) {
+		this.desc = desc;
+	}
+
+	public String toString() {
+		return this.version;
+	}
+}
