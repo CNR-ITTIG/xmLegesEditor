69c3fffd14f4a9589644c8e8762ca5e8abb7a520
Agnoloni <tommaso.agnoloni@gmail.com>
2007-10-02 15:40:41 +0000

    dalos Demo Branch
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1539 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/dom/meta/cnr/MetaCnrImpl.java
@@ -0,0 +1,154 @@
+package it.cnr.ittig.xmleges.editor.blocks.dom.meta.cnr;
+
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManagerException;
+import it.cnr.ittig.xmleges.core.services.document.EditTransaction;
+import it.cnr.ittig.xmleges.core.services.util.rulesmanager.UtilRulesManager;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+import it.cnr.ittig.xmleges.editor.services.dom.meta.cnr.MetaCnr;
+import it.cnr.ittig.xmleges.editor.services.dom.rinumerazione.Rinumerazione;
+import it.cnr.ittig.xmleges.editor.services.util.dom.NirUtilDom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class MetaCnrImpl implements MetaCnr, Loggable, Serviceable {
+
+	Logger logger;
+	
+	DocumentManager documentManager;
+
+	UtilRulesManager utilRulesManager;
+	
+	Rinumerazione rinumerazione; 
+	
+	NirUtilDom nirUtilDom;
+
+	
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+
+	}
+
+	public void service(ServiceManager serviceManager) throws ServiceException {		
+		rinumerazione = (Rinumerazione) serviceManager.lookup(Rinumerazione.class);
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);				
+		utilRulesManager = (UtilRulesManager) serviceManager.lookup(UtilRulesManager.class);
+		nirUtilDom = (NirUtilDom) serviceManager.lookup(NirUtilDom.class);
+	}
+	
+	public String[] getProprietario(Node node) {
+
+		Document doc = documentManager.getDocumentAsDom();
+		Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+		String strutturaEmanante=null;	
+		String autoritaEmanante=null;		
+		String tipoDestinatario=null;		
+		String strutturaDestinataria=null;
+		String tipo_provvedimento=null;
+		String choiceDipAreaDisciplina = null;
+		
+		
+		String tagDiscipline = null;
+		
+		Node n = UtilDom.findRecursiveChild(activeMeta,"cnr:meta");
+		
+		if (n!=null) {
+						
+			NodeList cnrMeta_elementList = n.getChildNodes();
+			for (int i = 0; i < cnrMeta_elementList.getLength();i++) {
+				String valore=UtilDom.getAttributeValueAsString(cnrMeta_elementList.item(i),"valore");
+				
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:strutturaEmanante"))
+					strutturaEmanante=valore;					
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:autoritaEmanante"))					
+					autoritaEmanante=valore;
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:tipoDestinatario"))
+					tipoDestinatario = valore;
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:strutturaDestinataria"))
+					strutturaDestinataria = valore;
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:tipoProvvedimento"))
+					tipo_provvedimento = valore;
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:dipartimento")){
+					tagDiscipline = valore;
+					choiceDipAreaDisciplina = "cnr:dipartimento";
+				}
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:areaScientifica")){
+					tagDiscipline = valore;
+					choiceDipAreaDisciplina = "cnr:areaScientifica";
+				}
+				if(cnrMeta_elementList.item(i).getNodeName().equals("cnr:disciplina")){
+					tagDiscipline = valore;
+					choiceDipAreaDisciplina = "cnr:disciplina";
+				}
+			}
+			return (new String[]{strutturaEmanante, autoritaEmanante, tipoDestinatario, strutturaDestinataria, tipo_provvedimento, tagDiscipline,choiceDipAreaDisciplina});
+		}else return null;
+		
+	}
+
+	public void setProprietario(Node node, String[] metadati) {
+		if(metadati!=null && metadati.length==7){
+			Document doc = documentManager.getDocumentAsDom();
+			try {
+				EditTransaction tr = documentManager.beginEdit();
+				if (setDOMCnr(node, metadati)) {
+					rinumerazione.aggiorna(doc);
+					documentManager.commitEdit(tr);
+				} else
+					documentManager.rollbackEdit(tr);
+				} catch (DocumentManagerException ex) {
+					logger.error(ex.toString() + " DocumentManagerException in MetaCnr");
+				}
+		}
+		
+	}
+
+	private boolean setDOMCnr(Node node, String[] metadati) {
+		
+			Document doc = documentManager.getDocumentAsDom();
+			Node activeMeta = nirUtilDom.findActiveMeta(doc,node);
+			Node proprietarioNode = UtilDom.findRecursiveChild(activeMeta,"proprietario");
+			
+			
+			boolean missingProprietario = false;
+			
+			if (proprietarioNode==null){
+				proprietarioNode = doc.createElementNS("http://www.cnr.it/provvedimenti/2.2","proprietario");
+				missingProprietario = true;
+			}
+			
+			Node cnrNode = UtilDom.findRecursiveChild(activeMeta,"cnr:meta");
+			if (cnrNode==null)
+			    cnrNode = utilRulesManager.getNodeTemplate("cnr:meta");
+			
+			String[] elementsName=new String[]{"cnr:strutturaEmanante","cnr:autoritaEmanante","cnr:tipoDestinatario","cnr:strutturaDestinataria","cnr:tipoProvvedimento",metadati[6]};
+			 
+			
+			UtilDom.removeAllChildren(cnrNode);
+			
+			for(int i=0;i<elementsName.length;i++){
+				Element toInsertElement = doc.createElement(elementsName[i]);
+				if((metadati[i]!=null)&&(!metadati[i].trim().equals("")))
+					UtilDom.setAttributeValue(toInsertElement,"valore",metadati[i]);
+				cnrNode.appendChild((Node)toInsertElement);	
+			}
+			utilRulesManager.orderedInsertChild(proprietarioNode,cnrNode);			
+			
+			if(missingProprietario){
+				Node metaNode = doc.getElementsByTagName("meta").item(0);
+				utilRulesManager.orderedInsertChild(metaNode,proprietarioNode);
+			}
+			return true;
+	}
+
+
+
+}
---
+++ xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/form/xmleges/linker/XmLegesLinkerFormImpl.java
@@ -0,0 +1,402 @@
+package it.cnr.ittig.xmleges.editor.blocks.form.xmleges.linker;
+
+import it.cnr.ittig.services.manager.Configurable;
+import it.cnr.ittig.services.manager.Configuration;
+import it.cnr.ittig.services.manager.ConfigurationException;
+import it.cnr.ittig.services.manager.Initializable;
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.form.Form;
+import it.cnr.ittig.xmleges.core.services.i18n.I18n;
+import it.cnr.ittig.xmleges.core.services.util.msg.UtilMsg;
+import it.cnr.ittig.xmleges.core.services.util.ui.UtilUI;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+import it.cnr.ittig.xmleges.core.util.file.UtilFile;
+import it.cnr.ittig.xmleges.editor.services.form.xmleges.linker.XmLegesLinkerForm;
+import it.cnr.ittig.xmleges.editor.services.xmleges.linker.XmLegesLinker;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+
+import org.w3c.dom.Node;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.form.parser.riferimenti.RiferimentiForm</code>.
+ * </h1>
+ * <h1>Gestione della form per l'avvio del parser dei riferimenti</h1>
+ * <h1>Configurazione:ParserRiferimentiFormImpl.xconfig - contiene l'elenco
+ * delle regioni con le relative urn</h1>
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.form.Form:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.util.ui.UtilUI:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.util.msg.UtilMsg:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.parser.riferimenti.ParserRiferimenti:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.document.DocumentManager:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li>xxx.yyy:</li>
+ * </ul>
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:sarti@dii.unisi.it">Lorenzo Sarti </a>, <a
+ *         href="mailto:mirco.taddei@gmail.com">Mirco Taddei </a>
+ */
+public class XmLegesLinkerFormImpl implements XmLegesLinkerForm, Loggable, Serviceable, Configurable, Initializable {
+	Logger logger;
+
+	Form form;
+
+	XmLegesLinker parser;
+
+	UtilUI utilUI;
+
+	DocumentManager dm;
+
+	UtilMsg utilMsg;
+
+	JComboBox regione, ente;
+
+	JTabbedPane tabbedPane;
+
+	JTextArea sorgente;
+
+	JTextArea risultati;
+	
+	AbstractButton analizza;
+
+	JRadioButton radioSel;
+
+	JRadioButton radioDoc;
+	
+	JCheckBox checkInt;
+	
+	JCheckBox checkInc;
+
+	String[] elencoregioni = new String[22];
+	
+	String[] elencoregioniUrn = new String[22];
+	
+	String[] elencoenti = new String[5];
+	
+	String[] elencoentiUrn = new String[5];
+
+	File fileRisultato;
+
+	String risultato;
+
+	boolean parseAll = false;
+
+	boolean parsedAll = false;
+
+	int tipo;
+
+	final static int TESTO = 0;
+
+	final static int NODO = 1;
+
+	final static int NODI = 2;
+
+	String text;
+
+	Node node;
+
+	Node[] nodes;
+
+	String risText;
+
+	String risNode;
+
+	String[] risNodes;
+
+	String risAll;
+
+	String error;
+
+	I18n i18n;
+	
+	String mesErr;
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		form = (Form) serviceManager.lookup(Form.class);
+		parser = (XmLegesLinker) serviceManager.lookup(XmLegesLinker.class);
+		utilUI = (UtilUI) serviceManager.lookup(UtilUI.class);
+		utilMsg = (UtilMsg) serviceManager.lookup(UtilMsg.class);
+		dm = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		i18n = (I18n) serviceManager.lookup(I18n.class);
+	}
+
+	// ////////////////////////////////////////////////// Configurable Interface
+	public void configure(Configuration configuration) throws ConfigurationException {
+		try {
+			Configuration[] elencoConf = configuration.getChildren();
+			int j=0, k=0;
+			for (int i = 0; i < elencoConf.length; i++){
+				if(elencoConf[i].getName().toLowerCase().startsWith("region")){
+				   elencoregioni[j] = elencoConf[i].getAttribute("name");
+				   elencoregioniUrn[j++] = elencoConf[i].getAttribute("urn");
+				}
+				else{
+				   elencoenti[k]= elencoConf[i].getAttribute("name");	
+				   elencoentiUrn[k++]= elencoConf[i].getAttribute("urn");
+				}
+			}
+		} catch (ConfigurationException e) {
+			logger.error("Impossibile leggere il file di configurazione");
+		}
+	}
+
+	// ///////////////////////////////////////////////// Initializable Interface
+	public void initialize() throws java.lang.Exception {
+		form.setMainComponent(this.getClass().getResourceAsStream("xmLegesLinker.jfrm"));
+		form.setSize(700, 500);
+		form.setName("editor.form.xmleges.link");
+		
+		form.setHelpKey("help.contents.form.xmlegeslinker");
+		
+		regione = (JComboBox) form.getComponentByName("editor.form.xmleges.link.regione");
+		ente = (JComboBox) form.getComponentByName("editor.form.xmleges.link.ente");
+		// FIXME si potrebbe mettere anche editabile ma al parser non si passa questa stringa ma la urn dell'ente selezionato
+		//ente.setEditable(true);
+		tabbedPane = (JTabbedPane) form.getComponentByName("editor.form.xmleges.link.tab");
+		sorgente = (JTextArea) form.getComponentByName("editor.form.xmleges.link.sorgente");
+		sorgente.setWrapStyleWord(true);
+		risultati = (JTextArea) form.getComponentByName("editor.form.xmleges.link.risultati");
+		risultati.setWrapStyleWord(true);
+		
+		radioDoc = (JRadioButton) form.getComponentByName("editor.form.xmleges.link.radio.intero");
+		radioDoc.setAction(new radioDocAction());
+		
+		radioSel = (JRadioButton) form.getComponentByName("editor.form.xmleges.link.radio.selezione");
+		radioSel.setAction(new radioSelAction());
+		
+		ButtonGroup grupporadio = new ButtonGroup();
+		grupporadio.add(radioDoc);
+		grupporadio.add(radioSel);
+		
+		
+		checkInt = (JCheckBox) form.getComponentByName("editor.form.xmleges.link.check.interni");
+		checkInc = (JCheckBox) form.getComponentByName("editor.form.xmleges.link.check.incompleti");
+		
+		analizza = (AbstractButton) form.getComponentByName("editor.form.xmleges.link.analizza");
+		analizza.setAction(new AnalizzaDocAction());
+		
+		regione.removeAllItems();
+		for (int i = 0; i < elencoregioni.length; i++)
+			regione.addItem(elencoregioni[i]);
+		
+		ente.removeAllItems();
+		for (int i = 0; i < elencoenti.length; i++)
+			ente.addItem(elencoenti[i]);
+		
+		mesErr = i18n.getTextFor("xmlegeslinker.errore");
+	}
+
+	// ///////////////////////////////////////// ParserRiferimentiForm Interface
+	public boolean openForm(String text) {
+		this.text = text;
+		this.tipo = TESTO;
+		setSorgente(text);
+		return openForm();
+	}
+
+	public boolean openForm(Node node) {
+		this.node = node;
+		this.tipo = NODO;
+		setSorgente(UtilDom.domToString(node, true));
+		return openForm();
+	}
+
+	public boolean openForm(Node[] nodes) {
+		this.nodes = nodes;
+		this.tipo = NODI;
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < nodes.length; i++) {
+			sb.append(UtilDom.domToString(nodes[i], true));
+			sb.append('\n');
+		}
+		setSorgente(sb.toString());
+		return openForm();
+	}
+
+	public void setParseAll(boolean all) {
+		this.parseAll = all;
+	}
+
+	public boolean isParseAll() {
+		return parsedAll;
+	}
+
+	public String getResultForText() {
+		return this.risText;
+	}
+
+	public String getResultForNode() {
+		return this.risNode;
+	}
+
+	public String[] getResultForNodes() {
+		return this.risNodes;
+	}
+
+	public String getResultForAll() {
+		return this.risAll;
+	}
+
+	protected boolean openForm() {
+		
+		if (!form.hasMainComponent())
+			try {
+				initialize();
+			} catch (Exception ex) {
+				logger.error(ex.toString(), ex);
+			}
+		radioSel.setEnabled(!parseAll);
+		radioDoc.setSelected(true);
+		checkInt.setEnabled(true);
+		checkInc.setEnabled(true);
+		checkInt.setSelected(true);
+		checkInc.setSelected(false);
+		regione.setSelectedIndex(0);
+		ente.setSelectedIndex(0);
+		form.showDialog();
+		return form.isOk();
+	}
+
+	protected void setSorgente(String text) {
+		tabbedPane.setSelectedIndex(0);
+		sorgente.setText(text);
+		sorgente.setCaretPosition(0);
+	}
+
+	protected void setRisultati(String text) {
+		tabbedPane.setSelectedIndex(1);
+		risultati.setText(text);
+		risultati.setCaretPosition(0);
+	}
+
+	protected class radioDocAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			checkInt.setEnabled(true);
+			checkInc.setEnabled(true);	
+		}
+	}
+	
+	protected class radioSelAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			checkInt.setEnabled(false);
+			checkInc.setEnabled(false);
+		}
+	}
+	
+	protected class AnalizzaDocAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			form.setDialogWaiting(true);
+			if(radioSel.isSelected())
+				AnalizzaSel();
+			else if(radioDoc.isSelected())
+				AnalizzaDoc();
+			form.setDialogWaiting(false);
+		}
+	}
+	
+	private void AnalizzaSel(){
+		form.setDialogWaiting(true);
+		try {
+			if (regione.getSelectedIndex() != 0)
+				parser.setRegione(elencoregioniUrn[regione.getSelectedIndex()]);
+			if (ente.getSelectedIndex() != 0)
+				parser.setEnte(elencoentiUrn[ente.getSelectedIndex()]);
+			parser.setEnabledRifIncompleti(false);
+			parser.setEnabledRifInterni(false);
+			switch (tipo) {
+			case TESTO:
+				risText = UtilFile.inputStreamToString(parser.parse(text));
+				error = parser.getError();
+				setRisultati(risText);
+				break;
+			case NODO:
+				risNode = UtilFile.inputStreamToString(parser.parse(node));
+				error = parser.getError();
+				setRisultati(risNode);
+				break;
+			case NODI:
+				risNodes = new String[nodes.length];
+				StringBuffer sb = new StringBuffer();
+				for (int i = 0; i < nodes.length; i++) {
+					risNodes[i] = UtilFile.inputStreamToString(parser.parse(nodes[i]));
+					error = parser.getError();
+					if (error != null)
+						break;
+					sb.append(risNodes[i]);
+					sb.append('\n');
+				}
+				setRisultati(sb.toString());
+				break;
+			}
+			utilMsg.msgInfo(form.getAsComponent(), "linker.analisi");
+			parsedAll = false;
+		} catch (Exception ex) {
+			logger.error(ex.toString(), ex);
+			utilMsg.msgError(form.getAsComponent(), mesErr + ex.toString());
+		}
+
+		if (error != null)			
+			utilMsg.msgError(form.getAsComponent(), mesErr + error);
+	}
+	
+	
+	private void AnalizzaDoc(){
+		try {
+			if (regione.getSelectedIndex() != 0)
+				parser.setRegione(elencoregioniUrn[regione.getSelectedIndex()]);
+			if (ente.getSelectedIndex() != 0)
+				parser.setEnte(elencoentiUrn[ente.getSelectedIndex()]);
+			parser.setEnabledRifIncompleti(checkInc.isSelected());
+			parser.setEnabledRifInterni(checkInt.isSelected());
+			risAll = UtilFile.inputStreamToString(parser.parse(dm.getRootElement()));
+			error = parser.getError();
+			setRisultati(risAll);
+			parsedAll = true;
+			utilMsg.msgInfo(form.getAsComponent(), "linker.analisicompleta");
+		} catch (Exception ex) {
+			logger.error(ex.toString(), ex); 
+			utilMsg.msgError(form.getAsComponent(), mesErr + ex.toString());
+		}
+
+		if (error != null) 
+			utilMsg.msgError(form.getAsComponent(), mesErr + error);
+	}
+
+}
