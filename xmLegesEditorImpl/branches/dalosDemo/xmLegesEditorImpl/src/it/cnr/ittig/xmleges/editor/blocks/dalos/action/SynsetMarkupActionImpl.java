eb701edfffed49e283c5fd64008febbd0cde164f
Agnoloni <tommaso.agnoloni@gmail.com>
2007-10-10 11:16:27 +0000

    aggiunte action e dom markatura synset
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1606 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/dalos/action/SynsetMarkupActionImpl.java
@@ -0,0 +1,165 @@
+package it.cnr.ittig.xmleges.editor.blocks.dalos.action;
+
+import it.cnr.ittig.services.manager.Initializable;
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.action.ActionManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentClosedEvent;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentOpenedEvent;
+import it.cnr.ittig.xmleges.core.services.event.EventManager;
+import it.cnr.ittig.xmleges.core.services.event.EventManagerListener;
+import it.cnr.ittig.xmleges.core.services.selection.SelectionChangedEvent;
+import it.cnr.ittig.xmleges.core.services.selection.SelectionManager;
+import it.cnr.ittig.xmleges.core.services.util.msg.UtilMsg;
+import it.cnr.ittig.xmleges.core.util.dom.UtilDom;
+import it.cnr.ittig.xmleges.editor.services.dalos.action.SynsetMarkupAction;
+import it.cnr.ittig.xmleges.editor.services.dalos.dom.SynsetMarkup;
+import it.cnr.ittig.xmleges.editor.services.dalos.objects.Synset;
+import it.cnr.ittig.xmleges.editor.services.util.dom.NirUtilDom;
+
+import java.awt.event.ActionEvent;
+import java.util.EventObject;
+
+import javax.swing.AbstractAction;
+
+import org.w3c.dom.Node;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.action.vigenza.VigenzaAction</code>.</h1>
+ * <h1>Descrizione</h1>
+ * <h1>Configurazione</h1>
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.form.Form:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li>xxx.yyy:</li>
+ * </ul>
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a> <a
+ *         href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni </a>
+ */
+
+public class SynsetMarkupActionImpl implements SynsetMarkupAction, Loggable, EventManagerListener, Serviceable, Initializable {
+	
+	Logger logger;
+
+//	ActionManager actionManager;
+
+//	EventManager eventManager;
+
+	DocumentManager documentManager;
+	
+	SelectionManager selectionManager;
+
+	SynsetMarkup synsetMarkup;
+	
+	NirUtilDom nirUtilDom;
+	
+	AbstractAction synsetMarkupAction = new synsetMarkupAction();
+	
+	Node activeNode;
+	
+	UtilMsg utilMsg;
+
+	int start, end;
+	
+	
+
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+//		actionManager = (ActionManager) serviceManager.lookup(ActionManager.class);
+//		eventManager = (EventManager) serviceManager.lookup(EventManager.class);
+		
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		nirUtilDom = (NirUtilDom) serviceManager.lookup(NirUtilDom.class);
+		selectionManager = (SelectionManager) serviceManager.lookup(SelectionManager.class);
+		synsetMarkup = (SynsetMarkup) serviceManager.lookup(SynsetMarkup.class);
+		utilMsg = (UtilMsg) serviceManager.lookup(UtilMsg.class);
+	}
+
+	// ///////////////////////////////////////////////// Initializable Interface
+	public void initialize() throws java.lang.Exception {
+//		actionManager.registerAction("editor.partizioni.vigenza", synsetMarkupAction);
+//		eventManager.addListener(this, SelectionChangedEvent.class);
+//		eventManager.addListener(this, DocumentOpenedEvent.class);
+//		eventManager.addListener(this, DocumentClosedEvent.class);
+//		synsetMarkupAction.setEnabled(false);
+	}
+
+	public void manageEvent(EventObject event) {
+		
+		if (event instanceof SelectionChangedEvent && !documentManager.isEmpty() && !nirUtilDom.isDtdBase()) {
+			activeNode = ((SelectionChangedEvent) event).getActiveNode();
+			logger.debug("enable vigenza");
+			start = ((SelectionChangedEvent) event).getTextSelectionStart();
+			end = ((SelectionChangedEvent) event).getTextSelectionEnd();
+		
+			synsetMarkupAction.setEnabled(synsetMarkup.canSetSynset(activeNode));//||vigenza.canSetVigenzaSpan(activeNode, start, end));
+
+		}
+		if (event instanceof DocumentClosedEvent || event instanceof DocumentOpenedEvent)
+			synsetMarkupAction.setEnabled(false);
+	}
+
+	
+	public void doSynsetMarkup(Synset synset) {
+		activeNode = selectionManager.getActiveNode();
+		int start = selectionManager.getTextSelectionStart();
+		int end = selectionManager.getTextSelectionEnd();
+	
+        if(synsetMarkup.canSetSynset(activeNode)){
+        	Node toselect = synsetMarkup.setSynset(activeNode, start, end, synset);
+        	setModified(toselect);
+        }else{
+        	utilMsg.msgError("editor.dalos.error.markup");
+        }
+	}
+	
+
+
+	// /////////////////////////////////////////////// Azioni
+	public class synsetMarkupAction extends AbstractAction {
+
+		public void actionPerformed(ActionEvent e) {
+//			if(activeNode!=null)
+//				doNewVigenza(activeNode);
+		}
+	}
+	
+	protected void setModified(Node modified) {
+		if (modified != null) {
+			selectionManager.setActiveNode(this, modified);
+			activeNode = modified;
+			synsetMarkupAction.setEnabled(synsetMarkup.canSetSynset(activeNode));
+			logger.debug(" set modified " + UtilDom.getPathName(modified));
+		} else
+			logger.debug(" modified null in set modified ");
+	}
+
+
+
+
+}
