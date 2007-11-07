6f951c0dbb10ccdbea24f17489743ca73b35835b
macchia <macchia@ittig.cnr.it>
2007-11-07 14:31:51 +0000

    Adattamento Editor per delibere, statuti e regolamenti
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1710 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/dom/meta/descrittori/MetaDescrittoriMaterie.java
+++ xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/dom/meta/descrittori/MetaDescrittoriMaterie.java
@@ -3,6 +3,7 @@ package it.cnr.ittig.xmleges.editor.services.dom.meta.descrittori;
 import org.w3c.dom.Node;
 
 import it.cnr.ittig.services.manager.Service;
+import it.ipiu.digest.parse.Vocabolario;
 
 public interface MetaDescrittoriMaterie extends Service{
 
