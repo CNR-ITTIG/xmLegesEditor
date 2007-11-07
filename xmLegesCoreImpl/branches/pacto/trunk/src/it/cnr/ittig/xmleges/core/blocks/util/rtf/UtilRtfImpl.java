d4149b6b2047b770d37544e13f567e917a0aebe3
macchia <macchia@ittig.cnr.it>
2007-11-07 15:16:03 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1717 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreImpl/branches/pacto/trunk/src/it/cnr/ittig/xmleges/core/blocks/util/rtf/UtilRtfImpl.java
@@ -0,0 +1,153 @@
+package it.cnr.ittig.xmleges.core.blocks.util.rtf;
+
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.util.rtf.UtilRtf;
+import it.cnr.ittig.xmleges.core.util.domwriter.DOMWriter;
+import it.cnr.ittig.xmleges.core.util.file.UtilFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.MimeConstants;
+
+import org.w3c.dom.Document;
+
+/**
+ * <h1>Implementazione del servizio <code>it.cnr.ittig.xmleges.editor.services.util.rtf.UtilRtf</code>.
+ * </h1>
+ * <h1>Descrizione</h1>
+ * <h1>Configurazione</h1>
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>dtd-rules-manager</li>
+ * <li>document-manager</li>
+ * <li>util-ui</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li>xxx.yyy:</li>
+ * </ul>
+ * 
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e Tecniche
+ * dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General Public
+ * License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:g.giardiello@gmail.com">Gerardo Giardiello </a>
+ */
+
+public class UtilRtfImpl implements UtilRtf, Loggable, Serviceable {
+	Logger logger;
+
+	DocumentManager documentManager;
+
+	UtilFile utilFile;
+
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+	}
+
+	/**
+	 * Trasforma un file XML in un file RTF utilizzando una trasformazione intermedia in
+	 * XSL-FO
+	 * 
+	 * @param xmlUri URI del dcoumento XML
+	 * @param xsltUri URI dello stylesheet XSL-FO
+	 * @param rtfUri URI dove salvare la versione del documento in RTF
+	 * 
+	 */
+	public void convertXML2RTF(String xmlUri, String xsltUri, String rtfUri) {
+
+		//Construct driver
+		FopFactory fopFactory = FopFactory.newInstance();
+
+		// Setup output
+		try {
+			OutputStream out = new java.io.FileOutputStream(new File(rtfUri));
+			
+			Fop fop = fopFactory.newFop(MimeConstants.MIME_RTF, out);
+
+			// Setup XSLT
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer(new StreamSource(new File(xsltUri)));
+
+			// Setup input for XSLT transformation
+			Source src = new StreamSource(new File(xmlUri));
+
+			// Resulting SAX events (the generated FO) must be piped through to FOP
+			Result res = new SAXResult(fop.getDefaultHandler());
+
+			// Start XSLT transformation and FOP processing
+			transformer.transform(src, res);
+			out.close();
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+
+	}
+
+	public void convertXML2RTF(Document doc, String xsltUri, String rtfUri) {
+		// Salvo il documento DOM su di un file temporaneo
+		try {
+			File temp = UtilFile.createTemp("export-rtf.xml");
+			if (saveDocument(temp))
+				convertXML2RTF(temp.getAbsolutePath(), xsltUri, rtfUri);
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+		}
+	}
+
+	private boolean saveDocument(File file) {
+		String encoding;
+		String defaultEncoding = "UTF-8";
+		if (documentManager.getEncoding() == null) {
+			logger.warn("No encoding found. Using default:" + defaultEncoding);
+			encoding = defaultEncoding;
+		} else
+			encoding = documentManager.getEncoding();
+		DOMWriter domWriter = new DOMWriter();
+		domWriter.setCanonical(false);
+		domWriter.setFormat(true);
+		try {
+			domWriter.setOutput(file, encoding);
+			domWriter.write(documentManager.getDocumentAsDom());
+			return true;
+		} catch (UnsupportedEncodingException ex) {
+			logger.error(ex.toString(), ex);
+		} catch (FileNotFoundException ex) {
+			logger.error(ex.toString(), ex);
+		}
+		return false;
+	}
+
+}
