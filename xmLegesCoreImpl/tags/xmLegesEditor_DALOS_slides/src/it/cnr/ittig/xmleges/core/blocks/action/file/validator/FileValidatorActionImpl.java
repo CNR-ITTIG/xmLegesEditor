698a587b79ae0a8f9ae53fe22f3b78d9da297e91
Lorenzo Bacci <lorenzo.bacci@icloud.com>
2007-12-05 12:39:56 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@1833 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreImpl/tags/xmLegesEditor_DALOS_slides/src/it/cnr/ittig/xmleges/core/blocks/action/file/validator/FileValidatorActionImpl.java
@@ -0,0 +1,186 @@
+package it.cnr.ittig.xmleges.core.blocks.action.file.validator;
+
+import it.cnr.ittig.services.manager.Initializable;
+import it.cnr.ittig.services.manager.Loggable;
+import it.cnr.ittig.services.manager.Logger;
+import it.cnr.ittig.services.manager.ServiceException;
+import it.cnr.ittig.services.manager.ServiceManager;
+import it.cnr.ittig.services.manager.Serviceable;
+import it.cnr.ittig.xmleges.core.services.action.ActionManager;
+import it.cnr.ittig.xmleges.core.services.action.file.open.FileOpenAction;
+import it.cnr.ittig.xmleges.core.services.action.file.save.FileSaveAction;
+import it.cnr.ittig.xmleges.core.services.action.file.validator.FileValidatorAction;
+import it.cnr.ittig.xmleges.core.services.bars.Bars;
+import it.cnr.ittig.xmleges.core.services.document.DocumentClosedEvent;
+import it.cnr.ittig.xmleges.core.services.document.DocumentManager;
+import it.cnr.ittig.xmleges.core.services.document.DocumentOpenedEvent;
+import it.cnr.ittig.xmleges.core.services.event.EventManager;
+import it.cnr.ittig.xmleges.core.services.event.EventManagerListener;
+import it.cnr.ittig.xmleges.core.services.i18n.I18n;
+import it.cnr.ittig.xmleges.core.services.util.msg.UtilMsg;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.util.EventObject;
+
+import javax.swing.AbstractAction;
+
+/**
+ * <h1>Implementazione del servizio
+ * <code>it.cnr.ittig.xmleges.editor.services.action.file.validator.FileValidatorAction</code>.</h1>
+ * <h1>Descrizione</h1>
+ * Questa implementazione registra nell'ActionManager l'aziona <code>file.validator</code>.
+ * <h1>Configurazione</h1>
+ * Nessuna
+ * <h1>Dipendenze</h1>
+ * <ul>
+ * <li>it.cnr.ittig.xmleges.editor.services.document.DocumentManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.event.EventManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.bars.Bars:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.action.file.open.FileOpenAction</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.action.ActionManager:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.action.file.save.FileSaveAction:1.0</li>
+ * <li>it.cnr.ittig.xmleges.core.services.i18n.I18n:1.0</li>
+ * <li>it.cnr.ittig.xmleges.editor.services.util.msg.UtilMsg:1.0</li>
+ * </ul>
+ * <h1>I18n</h1>
+ * <ul>
+ * <li><code>file.validator</code>: descrizione dell'azione come specificato nell'ActionManager;</li>
+ * <li><code>filevalidator.documentononvalido</code>: messaggio documento non valido;</li>
+ * <li><code>filevalidator.utilizzoxmleges</code>: messaggio sell'utilizzo di xmleges;</li>
+ * <li><code>filevalidator.controllo</code>: messaggio controllare la validit&agrave; del documento;</li>
+ * </ul>
+ * 
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e Tecniche
+ * dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General Public
+ * License </a></dd>
+ * </dl>
+ * @see it.cnr.ittig.xmleges.core.services.event.EventManager
+ * @see it.cnr.ittig.xmleges.core.services.action.ActionManager
+ * @see it.cnr.ittig.xmleges.core.services.i18n.I18n
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public class FileValidatorActionImpl implements FileValidatorAction, EventManagerListener, Loggable, Serviceable, Initializable {
+	Logger logger;
+
+	DocumentManager documentManager;
+
+	EventManager eventManager;
+
+	Bars bars;
+
+	ActionManager actionManager;
+
+	FileOpenAction fileOpenAction;
+
+	FileSaveAction fileSaveAction;
+
+	UtilMsg utilMsg;
+
+	ShowValidationMsgAction showValidationMsgAction = new ShowValidationMsgAction();
+
+	I18n i18n;
+	
+	// //////////////////////////////////////////////////// LogEnabled Interface
+	public void enableLogging(Logger logger) {
+		this.logger = logger;
+	}
+
+	// /////////////////////////////////////////////////// Serviceable Interface
+	public void service(ServiceManager serviceManager) throws ServiceException {
+		documentManager = (DocumentManager) serviceManager.lookup(DocumentManager.class);
+		eventManager = (EventManager) serviceManager.lookup(EventManager.class);
+		bars = (Bars) serviceManager.lookup(Bars.class);
+		actionManager = (ActionManager) serviceManager.lookup(ActionManager.class);
+		fileOpenAction = (FileOpenAction) serviceManager.lookup(FileOpenAction.class);
+		fileSaveAction = (FileSaveAction) serviceManager.lookup(FileSaveAction.class);
+		utilMsg = (UtilMsg) serviceManager.lookup(UtilMsg.class);
+		i18n = (I18n) serviceManager.lookup(I18n.class);
+	}
+
+	// ///////////////////////////////////////////////// Initializable Interface
+	public void initialize() throws java.lang.Exception {
+		eventManager.addListener(this, DocumentOpenedEvent.class);
+		eventManager.addListener(this, DocumentClosedEvent.class);
+		actionManager.registerAction("file.validator", showValidationMsgAction);
+		bars.getStatusBar().setText("file.validator", "file-validator");
+	}
+
+	// ////////////////////////////////////////// EventManagerListener Interface
+	public void manageEvent(EventObject event) {
+		if (event instanceof DocumentOpenedEvent)
+			showMessage(false);
+		else if (event instanceof DocumentClosedEvent)
+			bars.getStatusBar().setText("file.validator", "file-validator");
+	}
+
+	// //////////////////////////////////////////////// FileOpenAction Interface
+	public boolean doValidation() {
+		if (fileSaveAction.doSave())
+			return fileOpenAction.doOpen(documentManager.getSourceName(), false);
+		else
+			return false;
+	}
+
+	protected void showMessage(boolean yesNo) {
+		String[] errors = documentManager.getErrors();
+		bars.getStatusBar().setDefaultBackground("file-validator");
+		bars.getStatusBar().setDefaultForeground("file-validator");
+		if (documentManager.isEmpty())
+			bars.getStatusBar().setText("file.validator", "file-validator");
+		else if (documentManager.hasErrors()) {
+			bars.getStatusBar().setText("file.validator.novalid", "file-validator");
+			bars.getStatusBar().setBackground(Color.RED, "file-validator");
+			bars.getStatusBar().setForeground(Color.RED, "file-validator");
+			StringBuffer sb = new StringBuffer(i18n.getTextFor("filevalidator.documentononvalido"));
+			for (int i = 0; i < errors.length; i++) {
+				sb.append(errors[i]);
+				sb.append('\n');
+			}
+			if (!yesNo) {
+				sb.append(i18n.getTextFor("filevalidator.utilizzoxmleges"));
+				utilMsg.msgError(sb.toString());
+			} else {
+				sb.append(i18n.getTextFor("filevalidator.controllo"));
+				if (utilMsg.msgYesNo(sb.toString()))
+					doValidation();
+			}
+		} else
+			bars.getStatusBar().setText("file.validator.valid", "file-validator");
+	}
+
+	/**
+	 * Azione per la verifica del documento.
+	 * 
+	 * <p>
+	 * <dl>
+	 * <dt><b>Copyright &copy;: </b></dt>
+	 * <dd>2003 - 2004</dd>
+	 * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+	 * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+	 * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+	 * <dt><b>License: </b></dt>
+	 * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU General
+	 * Public License </a></dd>
+	 * </dl>
+	 * 
+	 * @version 1.0
+	 * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+	 * 
+	 */
+	protected class ShowValidationMsgAction extends AbstractAction {
+		public void actionPerformed(ActionEvent e) {
+			if (documentManager.hasErrors())
+				showMessage(true);
+		}
+	}
+
+}
