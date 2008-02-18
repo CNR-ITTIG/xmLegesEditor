896fb2f39b91f5dbf38b75bd2c8525293fba5b16
Lorenzo Bacci <lorenzo.bacci@icloud.com>
2008-02-18 09:56:02 +0000

    TreeOntoClass is observed now
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2006 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesEditorApi/branches/dalosDemo/xmLegesEditorApi/src/it/cnr/ittig/xmleges/editor/services/dalos/SynsetHolder.java
+++ xmLegesEditorApi/branches/dalosDemo/xmLegesEditorApi/src/it/cnr/ittig/xmleges/editor/services/dalos/SynsetHolder.java
@@ -1,11 +1,10 @@
 package it.cnr.ittig.xmleges.editor.services.dalos;
 
 import it.cnr.ittig.services.manager.Service;
-import it.cnr.ittig.xmleges.editor.services.dalos.objects.Synset;
 
 public interface SynsetHolder extends Service {
 	
-	public void setSynset(Synset syn);
+	public void setSynset(Object synObject);
 	
-	public Synset getSynset();
+	public Object getSynset();
 }
