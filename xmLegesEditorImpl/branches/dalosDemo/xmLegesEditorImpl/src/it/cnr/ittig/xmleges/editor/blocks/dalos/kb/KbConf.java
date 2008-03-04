e1d3ee7a8df72dc88e7b74111014e5b44ae380f8
Lorenzo Bacci <lorenzo.bacci@icloud.com>
2008-03-04 18:08:30 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2052 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/dalos/kb/KbConf.java
+++ xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/dalos/kb/KbConf.java
@@ -32,13 +32,14 @@ public class KbConf {
 
 	public static String ROOT_CLASS = "http://www.w3.org/2002/07/owl#Thing";
 	
-	public static String CONCEPTS = "concepts.owl";
-
+	//Specific language
 	public static String IND = "individuals.owl";
 	public static String INDW = "individuals-word.owl";
 	public static String TYPES = "types.owl";
 	public static String SOURCES = "sources.owl";
 	
+	//Common
+	public static String CONCEPTS = "concepts.owl";
 	public static String LOCAL_DOMAIN_ONTO = "consumer-law.owl";
 	public static String LOCAL_DOMAIN_MERGE_ONTO = "consumer-law-merge.owl";
 	public static String LOCAL_METALEVEL_ONTO = "owns.owl";
