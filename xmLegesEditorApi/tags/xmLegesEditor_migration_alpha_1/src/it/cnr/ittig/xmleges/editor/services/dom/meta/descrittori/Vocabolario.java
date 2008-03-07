318716864014ad6b2c7d4358b010e8ac0f9df1f6
Agnoloni <tommaso.agnoloni@gmail.com>
2008-03-07 15:11:21 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2062 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorApi/tags/xmLegesEditor_migration_alpha_1/src/it/cnr/ittig/xmleges/editor/services/dom/meta/descrittori/Vocabolario.java
@@ -0,0 +1,22 @@
+package it.cnr.ittig.xmleges.editor.services.dom.meta.descrittori;
+
+public class Vocabolario {
+	String nome;
+	String[] materie;
+	public String[] getMaterie() {
+		return materie;
+	}
+	public void setMaterie(String[] materie) {
+		this.materie = materie;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String toString() {
+		return nome;
+	}
+	
+}
