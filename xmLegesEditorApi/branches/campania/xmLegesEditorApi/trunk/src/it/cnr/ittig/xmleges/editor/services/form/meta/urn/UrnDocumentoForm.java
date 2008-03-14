3818e4ff285214011f106b2f62e72fa0f7889c55
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2008-03-14 09:57:29 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2095 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorApi/branches/campania/xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/form/meta/urn/UrnDocumentoForm.java
@@ -0,0 +1,52 @@
+package it.cnr.ittig.xmleges.editor.services.form.meta.urn;
+
+import it.cnr.ittig.services.manager.Service;
+import it.cnr.ittig.xmleges.editor.services.util.urn.Urn;
+
+/**
+ * Servizio per la gestione delle urn del documento NIR.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni</a>
+ */
+public interface UrnDocumentoForm extends Service {
+
+	/**
+	 * @param urn
+	 * @return <code>true</code> se la form &egrave; valida
+	 */
+	public boolean openForm(Urn[] urn);
+
+	/**
+	 * Apre la form per l'inserimento delle urn di un documento NIR.
+	 * 
+	 * @return <code>true</code> se la form &egrave; valida
+	 */
+	public boolean openForm();
+
+	/**
+	 * Restituisce le urn del documento.
+	 * 
+	 * @return urn del documento
+	 */
+	public Urn[] getUrn();
+
+	/**
+	 * Imposta le urn del documento
+	 * 
+	 * @param urn urn del documento
+	 */
+	public void setUrn(Urn[] urn);
+
+}
