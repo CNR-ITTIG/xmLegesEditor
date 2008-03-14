896a997288e39bf0b8240d9de9d0a50589b43981
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2008-03-14 08:39:26 +0000

    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2072 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreApi/branches/campania/xmLegesCoreApi/trunk/src/it/cnr/ittig/xmleges/core/services/spellcheck/SpellCheck.java
@@ -0,0 +1,127 @@
+package it.cnr.ittig.xmleges.core.services.spellcheck;
+
+import it.cnr.ittig.services.manager.Service;
+
+/**
+ * Servizio per la gestione del controllo ortografico del testo.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ * @author <a href="mailto:agnoloni@ittig.cnr.it">Tommaso Agnoloni </a>
+ */
+public interface SpellCheck extends Service {
+
+	/**
+	 * Restituisce la lista dei suggerimenti per una data parola
+	 * 
+	 * @param word
+	 *            parola di cui cercare i suggerimenti
+	 */
+	public String[] getSuggestions(String word);
+
+	/**
+	 * Esegue il controllo ortografico sul testo <code>text</code>.
+	 * 
+	 * @param text
+	 *            testo sul quale effettuare il controllo ortografico
+	 * @return elenco delle parole con errore
+	 */
+	public SpellCheckWord[] spellCheck(String text);
+
+	/**
+	 * Restituisce la lista dei dizionari <code>custom</code>.
+	 * 
+	 * @return lista nei nomi dei dizionari
+	 */
+	public String[] getDictionaries();
+
+	/**
+	 * Aggiunge il dizionario <code>custom</code> con il nome
+	 * <code>name</code>.
+	 * 
+	 * @param name
+	 *            nome del dizionazio
+	 */
+	public void addDictionary(String name);
+
+	/**
+	 * Rimuove il dizionario <code>custom</code> di nome <code>name</code>.
+	 * 
+	 * @param name
+	 *            nome del dizionario
+	 */
+	public void removeDictionary(String name);
+
+	/**
+	 * Imposta il dizionario <code>custom</code> con nome <code>name</code>.
+	 * 
+	 * @param name
+	 *            nome del dizionario
+	 */
+	public void setCustomDictionary(String name);
+
+	/**
+	 * Aggiunge la parola <code>word</code> al dizionario
+	 * <code>TEMPORARY_DICT</code> o al dizionario <code>PERSONAL_DICT</code>.
+	 * 
+	 * @param word
+	 *            parola da aggiungere
+	 * @param temporaryDict
+	 *            True: utilizza il TEMPORARY_DICT, False: utilizza il
+	 *            PERSONAL_DICT
+	 */
+	public void addWord(String word, boolean temporaryDict);
+
+	/**
+	 * Aggiunge il suggerimento <code>suggestion</code> alla parola
+	 * <code>word</code> nel dizionario <code>TEMPORARY_DICT</code> o nel
+	 * dizionario <code>PERSONAL_DICT</code>.
+	 * 
+	 * @param word
+	 *            parola errata
+	 * @param suggestion
+	 *            parola suggerita
+	 * @param temporaryDict
+	 *            True: utilizza il TEMPORARY_DICT, False: utilizza il
+	 *            PERSONAL_DICT
+	 */
+	public void addSuggestion(String word, String suggestion,boolean temporaryDict);
+
+	/**
+	 * Rimuove la parola <code>word</code> dal dizionario <code>custom</code>.
+	 * 
+	 * @param word
+	 *            parola da rimuovere
+	 */
+
+	public void removeWord(String word);
+
+	/**
+	 * Modifica la parola <code>oldWord</code> con <code>newWord</code>.
+	 * 
+	 * @param oldWord
+	 *            parola da modificare
+	 * @param newWord
+	 *            nuova parola da sostituire
+	 */
+	public void modifyWord(String oldWord, String newWord);
+
+	/**
+	 * Restituisce true se è caricata la libreria dello SpellCheck
+	 * 
+	 * @return <code>true</code> se è caricata la libreria
+	 */
+	public boolean isLoad();
+	
+}
---
+++ xmLegesCoreApi/branches/campania/xmLegesCoreApi/trunk/src/it/cnr/ittig/xmleges/core/services/util/rulesmanager/UtilRulesManager.java
@@ -0,0 +1,206 @@
+package it.cnr.ittig.xmleges.core.services.util.rulesmanager;
+
+import it.cnr.ittig.services.manager.Service;
+
+import java.util.Collection;
+
+import javax.swing.JMenu;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+/**
+ * Servizio per ...
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @version 1.1
+ * @author <a href="mailto:mollicone@ittig.cnr.it">Maurizio Mollicone </a>
+ */
+public interface UtilRulesManager extends Service {
+
+	/**
+	 * Costruisce un men&egrave; con la lista dei nodi inseribili nell'ordine
+	 * come: fratello successivo, figlio, fratello precedente del nodo
+	 * <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @return menu con la lista dei nodi inseribili
+	 */
+	public JMenu createMenuInsert(Node node);
+
+	/**
+	 * Costruisce un men&egrave; con la lista dei nodi inseribili come fratello
+	 * precedente del nodo <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @return menu con la lista dei nodi inseribili
+	 */
+	public JMenu createMenuInsertBefore(Node node);
+
+	/**
+	 * Costruisce un men&egrave; con la lista dei nodi inseribili come fratello
+	 * successivo del nodo <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @return menu con la lista dei nodi inseribili
+	 */
+	public JMenu createMenuInsertAfter(Node node);
+
+	/**
+	 * Costruisce un men&egrave; con la lista dei nodi inseribili fome figli del
+	 * nodo <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @return menu con la lista dei nodi inseribili
+	 */
+	public JMenu createMenuInsertNode(Node node);
+
+	/**
+	 * @param node nodo di riferimento
+	 * @param start inizio della selezione
+	 * @param end fine della selezione
+	 * @return menu con la lista dei nodi inseribili
+	 */
+	public JMenu createMenuInsert(Node node, int start, int end);
+
+	
+	/**
+	 * 
+	 * @param parent
+	 * @param new_Node
+	 * @return
+	 */
+	public boolean orderedInsertChild(Node parent, Node new_Node);
+	
+	
+	
+	/**
+	 * @param parent
+	 * @param child_node
+	 * @param new_Node
+	 * @return
+	 */
+	public boolean queryCanReplaceWith(Node parent, Node child_node, Node new_Node);
+
+	/**
+	 * @param node
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	public Collection queryInsertableNodeInText(Node node, int start, int end);
+
+	/**
+	 * @param node
+	 * @param start
+	 * @param end
+	 * @param newNode
+	 * @param replaceSelected
+	 * @return
+	 */
+	public boolean insertNodeInText(Node node, int start, int end, Node newNode, boolean replaceSelected);
+
+	/**
+	 * @param node
+	 * @param start
+	 * @param end
+	 * @param tagName
+	 * @return
+	 */
+	public Node encloseTextInTag(Node node, int start, int end, String tagName);
+
+	/**
+	 * @param node
+	 * @param start
+	 * @param end
+	 * @param tagName
+	 * @param NSprefix
+	 * @return
+	 */
+	public Node encloseTextInTag(Node node, int start, int end, String tagName, String NSprefix);
+
+	/**
+	 * @param text
+	 * @param tagName
+	 * @return
+	 */
+	public Node encloseTextInTag(String text, String tagName);
+
+	/**
+	 * @param text
+	 * @param tagName
+	 * @param NSprefix
+	 * @return
+	 */
+	public Node encloseTextInTag(String text, String tagName, String NSprefix);
+
+	/**
+	 * Fornisce un template del nodo minimale corrispondente all'elemento
+	 * <code>elem_name</code>
+	 * 
+	 * @param elem_name nome dell'elemento
+	 * @return il nodo di default per l'elemento passato
+	 */
+	public Node getNodeTemplate(String elem_name);
+
+	/**
+	 * Fornisce un template del nodo minimale corrispondente all'elemento
+	 * <code>elem_name</code> nel documento <code>doc</code>
+	 * 
+	 * @param doc il documento DOM associato
+	 * @param elem_name nome dell'elemento
+	 * @return il nodo di default per l'elemento passato
+	 */
+	public Node getNodeTemplate(Document doc, String elem_name);
+
+	/**
+	 * Indica se &egrave; possibile inserire un nodo con nome
+	 * <code>nodeName</code> come fratello precedente del nodo
+	 * <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @param nodeName nome del nodo da inserire
+	 * @return <code>true</code> se &egrave; possibile inserire il nodo
+	 */
+	public boolean canInsertBefore(Node node, String nodeName);
+
+	/**
+	 * Indica se &egrave; possibile inserire un nodo con nome
+	 * <code>nodeName</code> come fratello successivo del nodo
+	 * <code>node</code>.
+	 * 
+	 * @param node nodo di riferimento
+	 * @param nodeName nome del nodo da inserire
+	 * @return <code>true</code> se &egrave; possibile inserire il nodo
+	 */
+	public boolean canInsertAfter(Node node, String nodeName);
+
+	/**
+	 * Inserisce un sottoalbero alla posizione <code>pos</code> del nodo testo
+	 * <code>node</code><br>
+	 * Se il parametro <code>destructure</code> &egrave; <code>true</code>
+	 * l'inserimento tenter&agrave; di appiattire i livelli del sottoalbero fino
+	 * a quando &egrave; possibile inserirlo correttamente. In questo caso
+	 * alcune parti della struttura di partenza possono essere perse.
+	 * 
+	 * @param node nodo testo nel quale inserire il sottoalbero
+	 * @param pos posizione di inserimento
+	 * @param destructure <code>true</code> se deve essere eliminata la
+	 *        struttura
+	 * @return <code>true</code> se l'inserimento ha avuto successo
+	 */
+	public boolean insertSubTreeInText(Node node, int pos, boolean destructure);
+
+	
+
+}
