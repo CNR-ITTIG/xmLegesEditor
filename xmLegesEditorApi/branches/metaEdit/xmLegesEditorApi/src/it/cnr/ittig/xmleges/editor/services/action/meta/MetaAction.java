76917dcd48b690bf8c8e340bbd65bfbd4c114e00
Agnoloni <tommaso.agnoloni@gmail.com>
2008-03-19 16:22:20 +0000

    metaEdit branch
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2118 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesEditorApi/branches/metaEdit/xmLegesEditorApi/src/it/cnr/ittig/xmleges/editor/services/action/meta/MetaAction.java
@@ -0,0 +1,49 @@
+package it.cnr.ittig.xmleges.editor.services.action.meta;
+
+import it.cnr.ittig.services.manager.Service;
+
+/**
+ * Servizio per la gestione delle azioni per l'inserimento dei metadati di un
+ * documento NIR.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public interface MetaAction extends Service {
+
+	/**
+	 * Gestione dei metadati descrittori.
+	 */
+	public void doDescrittori();
+	
+	/**
+	 * Gestione del ciclo di vita del documento
+	 *
+	 */
+	public void doCiclodiVita();
+
+	/**
+	 * Gestione della Urn del documento.
+	 */
+	public void doUrn();
+	
+	/**
+	 * Gestione dei metadati inquadramento.
+	 */
+	public void doInquadramento();
+	/**
+	 * Gestione dei metadati materie.
+	 */
+	public void doMaterie();
+
+}
