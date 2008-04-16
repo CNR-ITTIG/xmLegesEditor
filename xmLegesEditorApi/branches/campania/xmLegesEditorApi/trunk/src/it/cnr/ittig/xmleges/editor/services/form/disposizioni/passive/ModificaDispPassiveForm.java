5fc031b5a1df271823155526092a60c0528d972b
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2008-04-16 08:06:07 +0000

    eliminato import inutile
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2207 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesEditorApi/branches/campania/xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/form/disposizioni/passive/ModificaDispPassiveForm.java
+++ xmLegesEditorApi/branches/campania/xmLegesEditorApi/trunk/src/it/cnr/ittig/xmleges/editor/services/form/disposizioni/passive/ModificaDispPassiveForm.java
@@ -3,7 +3,6 @@ package it.cnr.ittig.xmleges.editor.services.form.disposizioni.passive;
 import org.w3c.dom.Node;
 
 import it.cnr.ittig.services.manager.Service;
-import it.cnr.ittig.xmleges.editor.services.dom.vigenza.VigenzaEntity;
 
 /**
  * Servizio per la visualizzazione della form per la modifica/eliminazione delle disposizioni passive
