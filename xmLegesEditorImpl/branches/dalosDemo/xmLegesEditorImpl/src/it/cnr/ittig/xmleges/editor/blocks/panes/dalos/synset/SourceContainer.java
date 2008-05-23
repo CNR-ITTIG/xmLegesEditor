a50bf71106e752661b4824aff457fe7cb511b18f
Agnoloni <tommaso.agnoloni@gmail.com>
2008-05-23 09:55:55 +0000

    DALOS: highlight sources
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2296 010dc52d-3717-0410-9544-c3f04f3af1da

--- xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/panes/dalos/synset/SourceContainer.java
+++ xmLegesEditorImpl/branches/dalosDemo/xmLegesEditorImpl/src/it/cnr/ittig/xmleges/editor/blocks/panes/dalos/synset/SourceContainer.java
@@ -10,7 +10,11 @@ import it.cnr.ittig.xmleges.editor.services.dalos.objects.Synset;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.JEditorPane;
 import javax.swing.event.HyperlinkEvent;
@@ -30,6 +34,7 @@ public class SourceContainer extends JEditorPane implements HyperlinkListener{
 		
 		setEditable(false);
 		setContentType("text/html");
+		//setBackground(Color.YELLOW);
 		addHyperlinkListener(this);
 
 		
@@ -58,21 +63,33 @@ public class SourceContainer extends JEditorPane implements HyperlinkListener{
 			return;
 		}
 		
-		String html = "<html><body>" +
+		String html = "<html><body bgcolor=\"#EDE275\">" +
 			"</h3><h2><i>Sources</i></h2><table>"; 
 		
+		String evenBgColor = "#FFF380";
+		String oddBgColor =  "#EDE275";
+		String bgColor = evenBgColor;
+		int count = 0;
+		
 		for(Iterator i = synset.getSources().iterator(); i.hasNext();) {
+			
 			Source source = (Source) i.next();
 			String def = source.getContent();
+			
+			bgColor = count++ % 2==0?evenBgColor:oddBgColor;
+			
+	
 			if(def == null || def.trim().length() < 1) {
 				def = "Source text not available";
+			}else{
+				def = highlightDef(def, synset);
 			}
 			
 			// SETTARE LA URL
 			source.setLink("http://www.dalosproject.eu");
 			
-			html += "<tr><td><img src=\"./signature.png\"></td><td><i>" + 
-						def + "</i></td><td>&nbsp;</td><td>" +
+			html += "<tr bgcolor="+bgColor+"><td><img src=\"./signature.png\"></td><td><font face=\"Arial\">" + 
+						def + "</font></td><td>&nbsp;</td><td>" +
 						"<a href=\"" + source.getLink() + 
 						"\">" + source.getId() + "</a></td></tr>";			
 		}
@@ -85,6 +102,70 @@ public class SourceContainer extends JEditorPane implements HyperlinkListener{
 		
 		//System.out.println("DEBUG HTML: " + html + "\n");
 	}
+	
+	
+	
+	// una sola variante per ogni def ? 
+	
+	private String highlightDef(String def,Synset syn){
+		String highlighted;
+		int index;
+		List variants = Arrays.asList(syn.getVariants().toArray());
+				
+		// ordino le varianti dalla piu' lunga alla piu' corta
+		Collections.sort(variants,new Comparator() {
+		    public int compare(Object a, Object b ) {
+		    	  return(((String)b).length()-((String)a).length());
+		    }
+		});
+		
+		for(Iterator it = variants.iterator(); it.hasNext();){
+			String currentVariant = (String) it.next();
+			index = searchString(currentVariant, def);
+			//index = def.toLowerCase().indexOf(currentVariant.toLowerCase());
+			if(index!=-1){
+				highlighted = def.substring(0,index)+"<font style=\"background-color: #FF8040\">"+def.substring(index,index+currentVariant.length())+"</font>"+def.substring(index+currentVariant.length());
+				def = highlighted;
+			}
+		}
+		return def;
+	}
+	
+	
+	
+	// un po' di lacchezzi per ignorare gli spazi
+	private int searchString(String match, String text){
+		int idx = text.toLowerCase().indexOf(match.toLowerCase());
+		if(idx!=-1)
+			return idx;
+		
+		String[] tok = match.split("\\s+");
+		int start = text.toLowerCase().indexOf(tok[0].toLowerCase());
+		if(start!=-1){
+			idx = squeeze(text.substring(start)).toLowerCase().indexOf(squeeze(match).toLowerCase());
+			if(idx!=-1)
+				idx = start +idx;
+		}
+		return idx;
+	}
+	
+	
+	/**
+	 * @param str stringa da comprimere
+	 * @return la stringa compressa
+	 */
+	private String squeeze(String str) {
+		char[] buffer = str.toCharArray();
+		StringBuffer out = new StringBuffer(buffer.length);
+
+		for (int i = 0; i < buffer.length; i++) {
+			if (buffer[i] !=' ') {
+				out.append(buffer[i]);
+			}
+		}
+		return out.toString();
+	}
+	
 	
 	public void clearContent() {
 		
