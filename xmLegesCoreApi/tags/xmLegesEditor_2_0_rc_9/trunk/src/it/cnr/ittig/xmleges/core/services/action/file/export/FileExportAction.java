298c0ecf0b668a2398ac779832b67bbaeacff87f
gerardogiardiello <gerardo.giardiello@ittig.cnr.it>
2008-05-28 13:19:53 +0000

    rc9 (ufficiale)
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2328 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreApi/tags/xmLegesEditor_2_0_rc_9/trunk/src/it/cnr/ittig/xmleges/core/services/action/file/export/FileExportAction.java
@@ -0,0 +1,44 @@
+package it.cnr.ittig.xmleges.core.services.action.file.export;
+
+import it.cnr.ittig.services.manager.Service;
+
+/**
+ * Servizio per esportare il documento corrente.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public interface FileExportAction extends Service {
+
+	/**
+	 * Esporta il documento corrente nel browser.
+	 */
+	public boolean doExportBrowser();
+
+	/**
+	 * Esporta il documento corrente in HTML.
+	 */
+	public boolean doExportHTML();
+
+	/**
+	 * Esporta il documento corrente in PDF.
+	 */
+	public boolean doExportPDF();
+	
+	/**
+	 * Esporta il documento corrente in RTF.
+	 */
+	public boolean doExportRTF();
+
+
+}
---
+++ xmLegesCoreApi/tags/xmLegesEditor_2_0_rc_9/trunk/src/it/cnr/ittig/xmleges/core/services/document/DocumentManagerEvent.java
@@ -0,0 +1,51 @@
+package it.cnr.ittig.xmleges.core.services.document;
+
+import java.util.EventObject;
+
+import org.w3c.dom.Document;
+
+/**
+ * Classe base per definire gli eventi di modifica o selezione dei nodi del
+ * documento.
+ * <p>
+ * <dl>
+ * <dt><b>Copyright &copy;: </b></dt>
+ * <dd>2003 - 2004</dd>
+ * <dd><a href="http://www.ittig.cnr.it" target="_blank">Istituto di Teoria e
+ * Tecniche dell'Informazione Giuridica (ITTIG) <br>
+ * Consiglio Nazionale delle Ricerche - Italy </a></dd>
+ * <dt><b>License: </b></dt>
+ * <dd><a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GNU
+ * General Public License </a></dd>
+ * </dl>
+ * 
+ * @see it.cnr.ittig.xmleges.core.services.document.DocumentManager
+ * @version 1.0
+ * @author <a href="mailto:mirco.taddei@gmail.com">Mirco Taddei</a>
+ */
+public class DocumentManagerEvent extends EventObject {
+
+	/** Documento. */
+	Document document;
+
+	/**
+	 * Costruttore dell'evento.
+	 * 
+	 * @param source sorgente che ha emesso l'evento
+	 * @param document documento a cui &egrave; associato l'evento
+	 */
+	public DocumentManagerEvent(Object source, Document document) {
+		super(source);
+		this.document = document;
+	}
+
+	/**
+	 * Restituisce il documento modificato.
+	 * 
+	 * @return documento modificato
+	 */
+	public Document getDocument() {
+		return this.document;
+	}
+
+}
