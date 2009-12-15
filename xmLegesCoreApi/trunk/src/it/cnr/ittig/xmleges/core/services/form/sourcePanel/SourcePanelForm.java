dea0f8f839218ea817a4043da74e4f28d7716396
uccheddu <uccheddu@ittig.cnr.it>
2009-12-15 09:56:17 +0000

    Edit source XML
    git-svn-id: https://svn.ittig.cnr.it/svn/xmLegesEditor@2821 010dc52d-3717-0410-9544-c3f04f3af1da

---
+++ xmLegesCoreApi/trunk/src/it/cnr/ittig/xmleges/core/services/form/sourcePanel/SourcePanelForm.java
@@ -0,0 +1,13 @@
+package it.cnr.ittig.xmleges.core.services.form.sourcePanel;
+
+import it.cnr.ittig.services.manager.Service;
+
+public interface SourcePanelForm extends Service{
+
+	public boolean openForm();
+	
+	public void setSourceText(String text);
+	
+		
+
+}
